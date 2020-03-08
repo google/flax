b96b9567fd4e4f06a8de33e2fae01738e2865638
Marc van Zee <marcvanzee@gmail.com>
2020-03-08 18:40:43 +0100

    Added Github action for branches

--- howtos/scripts/generate_howto_branches_from_diffs.sh
+++ howtos/scripts/generate_howto_branches_from_diffs.sh
@@ -7,15 +7,6 @@ if [ -z "${GITHUB_TOKEN}" ]; then
     echo "error: GITHUB_TOKEN not found"
     exit 1
 fi
-if [ -z "${GITHUB_ACTOR}" ]; then
-    echo "error: GITHUB_ACTOR not found"
-    exit 1
-fi
-if [ -z "${GITHUB_REPOSITORY}" ]; then
-    echo "error: GITHUB_REPOSITORY not found"
-    exit 1
-fi
-
 
 # initialize git
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
@@ -27,38 +18,35 @@ git show-ref # useful for debugging
 git branch --verbose
 
 # publish any new files
-git checkout master
-git add -A
-timestamp=$(date -u)
-git commit -m "Automated publish: ${timestamp} ${GITHUB_SHA}" || exit 0
-git pull --rebase publisher master
-git push publisher master
+# git checkout master
+# git add -A
+# timestamp=$(date -u)
+# git commit -m "Automated publish: ${timestamp} ${GITHUB_SHA}" || exit 0
+# git pull --rebase publisher master
+# git push publisher master
 
+git checkout prerelease
 
+howto_diff_path="howtos/diffs"
 
-# git checkout prerelease
+top_dir=$(git rev-parse --show-toplevel)
 
-# # Change to top of repository.
-# cd $(git rev-parse --show-toplevel)
+cd "${top_dir}/${howto_diff_path}"
+howtos=$(ls *.diff | sed -e 's/.diff//')
+cd $top_dir
 
-# cd howtos
-# howtos=$(ls *.diff | sed -e 's/.diff//')
-# cd ..
-
-# git config --local user.email "action@github.com"
-# git config --local user.name "GitHub Action"
-
-# echo "Applying HOWTO diffs to branches..\n"
-# for howto in $howtos; do
-#   echo $howto
-#   if [[ -n $(git rev-parse --verify --quiet $howto) ]]; then
-#     echo "  Branch ${howto} exists already: overriding with diff"
-#     git branch -D $howto
-#   fi
-#   git checkout -b $howto
-#   git apply "howtos/${howto}.diff"
-#   git commit -am "Added howto branch ${howto}"
-# done
+echo "Applying HOWTO diffs to branches..\n"
+for howto in $howtos; do
+  echo $howto
+  if [[ -n $(git rev-parse --verify --quiet $howto) ]]; then
+    echo "  Branch ${howto} exists already: overriding with diff"
+    git branch -D $howto
+  fi
+  git checkout -b $howto
+  git apply "howtos/${howto}.diff"
+  git commit -am "Added howto branch ${howto}"
+  git push
+done
 
 
 
