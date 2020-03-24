b87ff87d0fba57aa2fa84524386cfcfa83940ed5
Adam Roberts <adarob@google.com>
2020-03-23 19:04:01 -0700

    init

--- examples/seq2seq/train.py
+++ examples/seq2seq/train.py
@@ -105,17 +105,34 @@ def decode_onehot(batch_inputs):
   return np.array(list(map(decode_inputs, batch_inputs)))
 
 
-class Encoder(nn.Module):
-  """LSTM encoder."""
+def get_sequence_lengths(sequence_batch, eos_id=1):
+  """Returns the length of each onehot sequence, including the EOS token."""
+  eos_row = sequence_batch[:, :, eos_id]
+  eos_idx = jnp.argmax(eos_row == eos_id, axis=-1)  # returns first occurence
+  return jnp.where(
+    eos_row[jnp.arange(eos_row.shape[0]), eos_idx] == eos_id,
+    eos_idx + 1,
+    sequence_batch.shape[1]  # if there is no EOS, use full length
+  )
 
-  def apply(self, inputs, hidden_size=512):
+
+class Encoder(nn.Module):
+  """LSTM encoder, returning final state."""
+
+  def apply(self, inputs, eos_id=1, hidden_size=512):
     # inputs.shape = (batch_size, seq_length, vocab_size).
     batch_size = inputs.shape[0]
-    carry = nn.LSTMCell.initialize_carry(jax.random.PRNGKey(0), (batch_size,),
-                                         hidden_size)
-    carry, _ = jax_utils.scan_in_dim(
-        nn.LSTMCell.partial(name='lstm'), carry, inputs, axis=1)
-    return carry
+    lengths = get_sequence_lengths(inputs, eos_id=eos_id)
+    carry = nn.LSTMCell.initialize_carry(
+        nn.make_rng(),
+        (batch_size,),
+        hidden_size)
+    _, outputs = jax_utils.scan_in_dim(
+        nn.LSTMCell.partial(name='lstm'),
+        carry,
+        inputs,
+        axis=1)
+    return outputs[jnp.arange(batch_size), jnp.maximum(0, lengths-1), :]
 
 
 class Decoder(nn.Module):
@@ -133,37 +150,84 @@ class Decoder(nn.Module):
 class Seq2seq(nn.Module):
   """Sequence-to-sequence class using encoder/decoder architecture."""
 
+  def _create_modules(self, eos_id, hidden_size):
+    encoder = Encoder.partial(
+        eos_id=eos_id, hidden_size=hidden_size).shared(name='encoder')
+    decoder = Decoder.shared(name='decoder')
+    return encoder, decoder
+
   def apply(self,
             encoder_inputs,
             decoder_inputs,
-            train=True,
-            max_output_len=None):
-    """Run the seq2seq model."""
-    # inputs.shape = (batch_size, seq_length, vocab_size).
+            eos_id=1,
+            hidden_size=512):
+    """Run the seq2seq model with teacher forcing.
+
+    Args:
+      encoder_inputs: padded batch of input sequences to encode, shaped
+        `[batch_size, max(encoder_input_lengths), vocab_size]`.
+      encoder_input_lengths: array of input sequence lengths, shaped
+        `[batch_size]`.
+      decoder_inputs: padded batch of expected decoded sequences for teacher
+        forcing, shaped `[batch_size, max(decoder_inputs_length), vocab_size]`.
+      eos_id: int, the token signalling when the end of a sequence is reached.
+      hidden_size: int, the number of hidden dimensions in the encoder and
+        decoder LSTMs.
+    """
+    encoder, decoder = self._create_modules(eos_id, hidden_size)
+
+    # Encode inputs
+    carry = encoder(encoder_inputs)
+    # Decode with teacher forcing.
+    _, x = decoder(carry, decoder_inputs[:, :-1])
+
+    return x
+
+  @nn.module_method
+  def sample(self,
+            encoder_inputs,
+            max_output_len,
+            sample_temperature=1.0,
+            eos_id=1,
+            hidden_size=512):
+    """Run the seq2seq model with sampling.
+
+    Args:
+      encoder_inputs: padded batch of input sequences to encode, shaped
+        `[batch_size, max(encoder_input_lengths), vocab_size]`.
+      max_output_len: int, maximum number of steps to decode. If None, decoding
+        will continue until all sequences reach EOS.
+      hidden_size: int, the number of hidden dimensions in the encoder and
+        decoder LSTMs.
+    """
     batch_size, _, vocab_size = encoder_inputs.shape
-    carry = Encoder(encoder_inputs, name='encoder')
-    decoder = Decoder.shared(name='decoder')
+    encoder, decoder = self._create_modules(eos_id, hidden_size)
 
-    # Teacher forcing.
-    if train:
-      _, x = decoder(carry, decoder_inputs[:, :-1])
-      return x
+    # Encode inputs
+    init_decoder_state = encoder(encoder_inputs)
 
-    # No teacher forcing, feeding actual output back into the decoder.
-    next_inputs = jnp.zeros((batch_size, vocab_size))
-    output = []
-    for _ in range(max_output_len):
-      next_inputs = next_inputs[:, np.newaxis]
-      carry, decoder_outputs = decoder(carry, next_inputs)
+    # Use autoregressive decoding, sampling predictions at each step.
+    def decode_step_fn(carry, unused_x):
+      decoder_carry, next_inputs = carry
+      decoder_carry, decoder_outputs = decoder(
+          decoder_carry, next_inputs[:, np.newaxis])
       decoder_outputs = decoder_outputs.squeeze()
-      output.append(decoder_outputs)
-      # Select the argmax as the next input.
-      next_inputs = jnp.equal(
-          decoder_outputs,
-          jnp.max(decoder_outputs, axis=-1)[:, None]
-      ).astype(jnp.float32)
-    return jnp.stack(output, axis=1)
+      predicted_tokens = jax.random.categorical(
+          nn.make_rng(), decoder_outputs / sample_temperature)
+      # Onehot encode predictions.
+      next_inputs = jnp.array(
+          predicted_tokens == jnp.arange(vocab_size),
+          dtype=jnp.float32)
 
+      return (decoder_carry, next_inputs), next_inputs
+
+    init_carry = (
+        init_decoder_state, # carry
+        jnp.zeros((batch_size, vocab_size), dtype=np.float32), # next_inputs
+    )
+    predictions = jax_utils.scan_in_dim(
+        decode_step_fn, init_carry, xs=[None]*max_output_len, axis=1)
+    return predictions
 
 def create_model(rng):
   """Creates a seq2seq model."""
