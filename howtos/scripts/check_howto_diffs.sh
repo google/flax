9f1a9ef338ea0ec7212de8d324857d462ffeab3b
Daniel Suo <danielsuo@gmail.com>
2020-05-15 12:34:03 +0000

    Fix up per @marcvanzee requested changes

--- howtos/scripts/check_howto_diffs.sh
+++ howtos/scripts/check_howto_diffs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
-# Checks the following for each HOWTO diff in howtos/diffs
+# Checks the following for each HOWTO diff in howtos/diffs:
 # 1. Check if diffs are stale and can no longer be applied to *this branch*
 # 2. Apply each diff in turn and run appropriate unit tests in *this branch*
+
+# TODO (dsuo): refactor duplicate code from check, pack, and apply_howto_diffs.
 
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo
@@ -18,7 +20,7 @@ cd $top_dir
 
 # Check for stale diffs first before running any tests. This duplicates some
 # logic, but should speed up testing.
-printf "Verify all HOWTO diffs can be applied...\n"
+printf "Verifying all HOWTO diffs can be applied...\n"
 
 for howto in $howtos; do
   diff_file="${howto_diff_path}/${howto}.diff"
@@ -31,31 +33,31 @@ for howto in $howtos; do
   fi
 done
 
-printf "Run unit tests for each diff...\n"
+printf "Running unit tests for each diff...\n"
 
-# Fetch all branches
+# Fetch all branches.
 git fetch --all
 
+curr_branch="$(git rev-parse --abbrev-ref HEAD)"
 for howto in $howtos; do
   diff_file="${howto_diff_path}/${howto}.diff"
-  curr_branch="$(git rev-parse --abbrev-ref HEAD)"
 
-  # Delete local howto branch if already exists in case we're running locally
+  # Delete local howto branch if already exists in case we're running locally.
   git branch -D $howto || true
   git checkout -b $howto
   git apply $diff_file
 
-  # Run unit test on affected examples only
+  # Run unit test on affected examples only.
   if ! git diff --name-only $curr_branch | xargs dirname | xargs pytest; then
     printf "\nERROR: Tests failed for howto ${howto}! ==> PLEASE FIX HOWTO\n"
 
-    # Undo patch in case we're running locally
+    # Undo patch in case we're running locally.
     git apply -R $diff_file
     git checkout $curr_branch
     exit 1
   fi
 
-  # Undo patch so we can run the next test
+  # Undo patch so we can run the next test.
   git apply -R $diff_file
   git checkout $curr_branch
 done
