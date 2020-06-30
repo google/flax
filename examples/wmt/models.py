b566e59f223357af384943fb9e1c2ef930350ca5
Anselm Levskaya <levskaya@google.com>
2020-06-30 12:10:07 -0700

    fix flax autoregressive cache initialization for upcoming JAX changes.
    PiperOrigin-RevId: 319071417

--- examples/wmt/models.py
+++ examples/wmt/models.py
@@ -118,7 +118,7 @@ class AddPositionEmbs(nn.Module):
     # We only use the cache's position index for tracking decoding position.
     if cache:
       if self.is_initializing():
-        cache.store(lambda: (4, (1, 1)))
+        cache.store(np.array((4, 1, 1), dtype=np.int32))
       else:
         cache_entry = cache.retrieve(None)
         i = cache_entry.i
