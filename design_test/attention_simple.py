0ef23240438f3af73fe87a7483051a7feffc7f98
jheek <heekjonathan@gmail.com>
2020-07-02 07:59:28 +0000

    transformed attention

--- design_test/attention_simple.py
+++ design_test/attention_simple.py
@@ -11,11 +11,17 @@ from flax import nn
 
 
 def softmax_attn(scope: Scope, weights: Array):
+  del scope
   norm_dims = tuple(range(weights.ndim // 2, weights.ndim))
   log_norms = jax.scipy.special.logsumexp(
       weights, axis=norm_dims, keepdims=True)
   return jnp.exp(weights - log_norms)
 
+def with_dropout(fn, rate: float, deterministic: bool = False):
+  def attn_fn(scope: Scope, weights: Array):
+    attn_weights = fn(scope, weights)
+    return nn.dropout(scope, attn_weights, deterministic=deterministic, rate=rate)
+  return attn_fn
 
 def _dot_product_attention(
     scope: Scope,
@@ -50,10 +56,13 @@ def dot_product_attention(
     inputs_kv: Array,
     bias: Optional[Array] = None,
     qkv_features: Optional[int] = None,
+    out_features: Optional[int] = None,
     attn_fn: Callable = softmax_attn,
     dtype=jnp.float32):
   if qkv_features is None:
     qkv_features = inputs_q.shape[-1]
+  if out_features is None:
+    out_features = inputs_q.shape[-1]
   dense = partial(nn.dense, features=qkv_features, bias=False, dtype=dtype)
 
   query = scope.child(dense, 'query')(inputs_q)
@@ -64,8 +73,8 @@ def dot_product_attention(
       scope, query, key, value,
       bias=bias,
       attn_fn=attn_fn, dtype=dtype)
-  
-  return y
+
+  return scope.child(nn.dense, 'out')(y, features=out_features, dtype=dtype)
 
 
 
@@ -74,12 +83,13 @@ def multi_head_dot_product_attention(
     inputs_q: Array,
     inputs_kv: Array,
     bias: Optional[Array] = None,
-    qkv_features = None,
-    out_features = None,
+    qkv_features: Optional[int] = None,
+    out_features: Optional[int] = None,
     attn_fn: Callable = softmax_attn,
     batch_axes: Sequence[int] = (0,),
     num_heads: int = 1,
-    dtype=jnp.float32):
+    dtype=jnp.float32,
+    broadcast_dropout=False):
 
   if qkv_features is None:
     qkv_features = inputs_q.shape[-1]
@@ -90,6 +100,7 @@ def multi_head_dot_product_attention(
       dot_product_attention,
       attn_fn=attn_fn,
       qkv_features=qkv_features // num_heads,
+      out_features=out_features,
       dtype=dtype)
   attn_fn = lift.vmap(
       attn_fn,
@@ -97,27 +108,27 @@ def multi_head_dot_product_attention(
       axis_size=num_heads,
       variable_in_axes={'param': 0},
       variable_out_axes={'param': 0},
-      split_rngs={'param': True})
+      split_rngs={'param': True, 'dropout': not broadcast_dropout})
   for axis in reversed(sorted(batch_axes)):
     attn_fn = lift.vmap(
         attn_fn,
         in_axes=(axis, axis, axis), out_axes=axis,
         variable_in_axes={'param': None},
         variable_out_axes={'param': None},
-        split_rngs={'param': False})
+        split_rngs={'param': False, 'dropout': not broadcast_dropout})
 
-  y = scope.child(attn_fn, 'attn')(inputs_q, inputs_kv, bias)
-  print(y.shape)
-  return scope.child(nn.dense_general, 'out')(
-      y, out_features,
-      axis=(-2, -1),
-      dtype=dtype)
+  y = attn_fn(scope, inputs_q, inputs_kv, bias)
+  return y.mean(axis=-2)
 
 inputs = jnp.ones((2, 7, 16))
 
 y, variables = init(multi_head_dot_product_attention)(
-    random.PRNGKey(0), inputs, inputs,
+    {'param': random.PRNGKey(0), 'dropout': random.PRNGKey(1)},
+    inputs, inputs,
     num_heads=2,
-    batch_axes=(0,))
+    batch_axes=(0,),
+    attn_fn=with_dropout(softmax_attn, 0.1, deterministic=False)
+    )
+
 print(y.shape)
 print(jax.tree_map(jnp.shape, variables))
