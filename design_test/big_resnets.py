921f11b693a124c211988258fdd7a1022fffd13a
jheek <heekjonathan@gmail.com>
2020-07-03 08:58:17 +0000

    generalize

--- design_test/big_resnets.py
+++ design_test/big_resnets.py
@@ -1,6 +1,6 @@
 # this example demonstrates how to build big networks using rematerialzation and scan to reduce memory footprint compilation size. 
 
-
+import numpy as np
 
 from flax.core import Scope, init, apply, unfreeze, lift
 
@@ -33,7 +33,31 @@ def residual_block(scope: Scope, x: Array, conv, norm, act, features: int):
   return act(residual + x)
 
 
-def big_resnet(scope: Scope, x, num_blocks=10, num_superblocks=10, dtype=jnp.float32,
+# general purpose remat scan primitive
+def remat_scan(body_fn, scope, x, lengths, variable_modes, split_rngs):
+  if len(lengths) == 1:
+    def wrapper(scope, x, _):
+      return body_fn(scope, x), ()
+
+    x, _ = lift.scan(
+        wrapper, scope, x, (),
+        length=lengths[0],
+        variable_modes=variable_modes,
+        split_rngs=split_rngs)
+  else:
+    @lift.remat
+    def inner_loop(scope, x, _):
+      x = remat_scan(body_fn, scope, x, lengths[1:], variable_modes, split_rngs)
+      return x, ()
+    x, _ = lift.scan(
+        inner_loop, scope, x, (),
+        length=lengths[0],
+        variable_modes=variable_modes,
+        split_rngs=split_rngs)
+  return x
+
+
+def big_resnet(scope: Scope, x, blocks=(10, 10), dtype=jnp.float32,
                norm=default_norm, act=nn.relu):
   conv = partial(nn.conv, bias=False, dtype=dtype)
   norm = partial(norm, dtype=dtype)
@@ -41,27 +65,15 @@ def big_resnet(scope: Scope, x, num_blocks=10, num_superblocks=10, dtype=jnp.flo
   # a two stage resnet where inner blocks are rematerialized to make sure
   # memory consumtion grows as O(sqrt(N)) and compute is O(N) where N is the number of blocks..
   # we use a double scan such that the compiled binary is of size O(1).
+  print('total residual blocks:', np.prod(blocks))
 
-  print('total residual blocks:', num_superblocks * num_blocks)
+  def body_fn(scope, x):
+    return residual_block(scope, x, conv, norm, act, features=x.shape[-1])
 
-  @lift.remat
-  def super_block(scope, x, _):
-    def block(scope, x, _):
-      return residual_block(scope, x, conv, norm, act, features=x.shape[-1]), ()
-    x, _ = lift.scan(
-        block, scope, x, (),
-        length=num_blocks,
-        variable_modes={'param': 'scan', 'batch_stats': 'scan'},
-        split_rngs={'param': True})
-    return x, ()
-
-  x, _ = lift.scan(
-      super_block, scope, x, (),
-      length=num_superblocks,
+  return remat_scan(
+      body_fn, scope, x, lengths=blocks,
       variable_modes={'param': 'scan', 'batch_stats': 'scan'},
       split_rngs={'param': True})
-
-  return x
 
 if __name__ == "__main__":
   x = random.normal(random.PRNGKey(0), (1, 8, 8, 8))
