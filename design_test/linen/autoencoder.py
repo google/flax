375a58a7aa8946c415e9f1642f74e4f32a5b9f97
Avital Oliver <avital@thewe.net>
2020-07-14 16:01:20 +0200

    Add comments and a bit of top-level patterns

--- design_test/linen/autoencoder.py
+++ design_test/linen/autoencoder.py
@@ -7,6 +7,7 @@ from flax.linen import Module, MultiModule
 import numpy as np
 
 
+# A simple Dense module
 class Dense(Module):
   features: int
   kernel_init: Callable = initializers.lecun_normal()
@@ -17,6 +18,7 @@ class Dense(Module):
     return jnp.dot(x, kernel) + self.param('bias', self.bias_init, (self.features,))
 
 
+# A concise MLP defined via lazy submodule initialization
 class MLP(Module):
   widths: Iterable
 
@@ -26,37 +28,64 @@ class MLP(Module):
     return Dense(self, self.widths[-1])(x)
 
 
+# An autoencoder exposes multiple methods, so we extend from MultiModule
+# and define all submodules in setup().
 class AutoEncoder(MultiModule):
   encoder_widths: Iterable
   decoder_widths: Iterable
   input_shape: Tuple = None
 
   def setup(self):
-    self._encoder = MLP(self, self.encoder_widths)
-    self._decoder = MLP(self, self.decoder_widths + (jnp.prod(self.input_shape), ))
+    # Submodules attached in `setup` get names via attribute assignment
+    self.encoder = MLP(self, self.encoder_widths)
+    self.decoder = MLP(self, self.decoder_widths + (jnp.prod(self.input_shape), ))
 
   def __call__(self, x):
     return self.decode(self.encode(x))
 
   def encode(self, x):
     assert x.shape[-len(self.input_shape):] == self.input_shape
-    return self._encoder(jnp.reshape(x, (x.shape[0], -1)))
+    return self.encoder(jnp.reshape(x, (x.shape[0], -1)))
 
   def decode(self, z):
-    z = self._decoder(z)
+    z = self.decoder(z)
     x = nn.sigmoid(z)
     x = jnp.reshape(x, (x.shape[0],) + self.input_shape)
     return x
 
 
+# `ae` is a detached module, which has no variables.
 ae = AutoEncoder(
   parent=None,
   encoder_widths=(32, 32, 32),
   decoder_widths=(32, 32, 32),
   input_shape=(28, 28, 1))
+
+
+# `ae.initialized` returnes a materialized copy of `ae` by
+# running through an input to create submodules defined lazily.
 ae = ae.initialized(
   {'param': random.PRNGKey(42)},
   jnp.ones((1, 28, 28, 1)))
+
+
+# Now you can use `ae` as a normal object, calling any methods defined on AutoEncoder
 print("reconstruct", jnp.shape(ae(jnp.ones((1, 28, 28, 1)))))
 print("encoder", jnp.shape(ae.encode(jnp.ones((1, 28, 28, 1)))))
+
+
+# `ae.variables` is a frozen dict that looks like
+# {"param": {"decoder": {"Dense_0": {"bias": ..., "kernel": ...}, ...}}
 print("var shapes", jax.tree_map(jnp.shape, ae.variables))
+
+
+# You can access submodules defined in setup(), they are just references on
+# the autoencoder instance
+encoder = ae.encoder
+print("encoder var shapes", jax.tree_map(jnp.shape, encoder.variables))
+
+
+# You can also acccess submodules that were defined in-line.
+# (We may add syntactic sugar here, e.g. to allow `ae.encoder.Dense_0`)
+encoder_dense0 = ae.encoder.children['Dense_0']
+print("encoder dense0 var shapes", jax.tree_map(jnp.shape, encoder_dense0.variables))
