c28e258fe4dd51b0edfb6bc9c3502928fbfc6ea8
Anselm Levskaya <levskaya@google.com>
2020-08-11 22:09:22 -0700

    untested conversion of improved imagenet models to linen

--- linen_examples/imagenet/models.py
+++ linen_examples/imagenet/models.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Flax implementation of ResNet V1.
 """
 
@@ -22,86 +21,113 @@ from flax import linen as nn
 import jax.numpy as jnp
 
 from functools import partial
-from typing import Any
+from typing import Any, Callable, Sequence
+
+ModuleDef = Any
 
 
-class ResidualBlock(nn.Module):
+class ResNetBlock(nn.Module):
+  """ResNet block."""
+  filters: int
+  strides: (int, int) = (1, 1)
+  conv: ModuleDef
+  norm: ModuleDef
+  act: Callable
+
+  @nn.compact
+  def __call__(self, x,):
+    residual = x
+    y = self.conv(self.filters, (3, 3), self.strides)(x)
+    y = self.norm()(y)
+    y = self.act(y)
+    y = self.conv(self.filters, (3, 3))(y)
+    y = self.norm(scale_init=nn.initializers.zeros)(y)
+
+    if residual.shape != y.shape:
+      residual = self.conv(self.filters, (1, 1),
+                           self.strides, name='conv_proj')(residual)
+      residual = self.norm(name='norm_proj')(residual)
+
+    return self.act(residual + y)
+
+
+class BottleneckResNetBlock(nn.Module):
   """Bottleneck ResNet block."""
   filters: int
   strides: (int, int) = (1, 1)
-  train: bool = True
-  dtype: Any = jnp.float32
+  conv: ModuleDef
+  norm: ModuleDef
+  act: Callable
 
   @nn.compact
   def __call__(self, x):
-    needs_projection = x.shape[-1] != self.filters * 4 or self.strides != (1, 1)
-    batch_norm = partial(nn.BatchNorm, use_running_average=not self.train,
-                                       momentum=0.9, epsilon=1e-5,
-                                       dtype=self.dtype)
-    conv = partial(nn.Conv, use_bias=False, dtype=self.dtype)
-
     residual = x
-    if needs_projection:
-      residual = conv(self.filters * 4, (1, 1), self.strides, name='proj_conv')(residual)
-      residual = batch_norm(name='proj_bn')(residual)
+    y = self.conv(self.filters, (1, 1))(x)
+    y = self.norm()(y)
+    y = self.act(y)
+    y = self.conv(self.filters, (3, 3), self.strides)(y)
+    y = self.norm()(y)
+    y = self.act(y)
+    y = self.conv(self.filters * 4, (1, 1))(y)
+    y = self.norm(scale_init=nn.initializers.zeros)(y)
 
-    y = conv(self.filters, (1, 1), name='conv1')(x)
-    y = batch_norm(name='bn1')(y)
-    y = nn.relu(y)
-    y = conv(self.filters, (3, 3), self.strides, name='conv2')(y)
-    y = batch_norm(name='bn2')(y)
-    y = nn.relu(y)
-    y = conv(self.filters * 4, (1, 1), name='conv3')(y)
+    if residual.shape != y.shape:
+      residual = self.conv(self.filters * 4, (1, 1),
+                           self.strides, name='conv_proj')(residual)
+      residual = self.norm(name='norm_proj')(residual)
 
-    y = batch_norm(name='bn3', scale_init=nn.initializers.zeros)(y)
-    y = nn.relu(residual + y)
-    return y
+    return self.act(residual + y)
 
 
 class ResNet(nn.Module):
   """ResNetV1."""
+  stage_sizes: Sequence[int]
+  block_cls: Sequence[int]
   num_classes: int
   num_filters: int = 64
-  num_layers: int = 50
-  train: bool = True
   dtype: Any = jnp.float32
+  act: Callable = nn.relu
+  train: bool = True
 
   @nn.compact
   def __call__(self, x):
-    if self.num_layers not in _block_size_options:
-      raise ValueError('Please provide a valid number of layers')
-    block_sizes = _block_size_options[self.num_layers]
-    x = nn.Conv(self.num_filters, (7, 7), (2, 2),
-                padding=[(3, 3), (3, 3)],
-                use_bias=False,
-                dtype=self.dtype,
-                name='init_conv')(x)
-    x = nn.BatchNorm(use_running_average=not self.train,
-                     momentum=0.9, epsilon=1e-5,
-                     dtype=self.dtype,
-                     name='init_bn')(x)
+    conv = partial(nn.Conv, use_bias=False, dtype=self.dtype)
+    norm = partial(nn.BatchNorm,
+                   use_running_average=not self.train,
+                   momentum=0.9,
+                   epsilon=1e-5,
+                   dtype=self.dtype)
+
+    x = conv(self.num_filters, (7, 7), (2, 2),
+             padding=[(3, 3), (3, 3)],
+             name='conv_init')(x)
+    x = norm(name='bn_init')(x)
+    x = nn.relu(x)
     x = nn.max_pool(x, (3, 3), strides=(2, 2), padding='SAME')
-    for i, block_size in enumerate(block_sizes):
+    for i, block_size in enumerate(self.stage_sizes):
       for j in range(block_size):
         strides = (2, 2) if i > 0 and j == 0 else (1, 1)
-        x = ResidualBlock(self.num_filters * 2 ** i,
-                          strides=strides,
-                          train=self.train,
-                          dtype=self.dtype)(x)
+        x = self.block_cls(self.num_filters * 2 ** i,
+                           strides=strides,
+                           conv=conv,
+                           norm=norm,
+                           act=self.act)(x)
     x = jnp.mean(x, axis=(1, 2))
     x = nn.Dense(self.num_classes, dtype=self.dtype)(x)
-    x = jnp.asarray(x, jnp.float32)
+    x = jnp.asarray(x, self.dtype)
     x = nn.log_softmax(x)
     return x
 
 
-# a dictionary mapping the number of layers in a resnet to the number of blocks
-# in each stage of the model.
-_block_size_options = {
-    18: [2, 2, 2, 2],
-    34: [3, 4, 6, 3],
-    50: [3, 4, 6, 3],
-    101: [3, 4, 23, 3],
-    152: [3, 8, 36, 3],
-    200: [3, 24, 36, 3]
-}
+ResNet18 = partial(ResNet, stage_sizes=[2, 2, 2, 2],
+                   block_cls=ResNetBlock)
+ResNet34 = partial(ResNet, stage_sizes=[3, 4, 6, 3],
+                   block_cls=ResNetBlock)
+ResNet50 = partial(ResNet, stage_sizes=[3, 4, 6, 3],
+                   block_cls=BottleneckResNetBlock)
+ResNet101 = partial(ResNet, stage_sizes=[3, 4, 23, 3],
+                    block_cls=BottleneckResNetBlock)
+ResNet152 = partial(ResNet, stage_sizes=[3, 8, 36, 3],
+                    block_cls=BottleneckResNetBlock)
+ResNet200 = partial(ResNet, stage_sizes=[3, 24, 36, 3],
+                    block_cls=BottleneckResNetBlock)
--- linen_examples/imagenet/train.py
+++ linen_examples/imagenet/train.py
@@ -87,7 +87,7 @@ def model(**kwargs):
       model_dtype = jnp.float16
   else:
     model_dtype = jnp.float32
-  return models.ResNet(num_classes=1000, dtype=model_dtype, **kwargs)
+  return models.ResNet50(num_classes=1000, dtype=model_dtype, **kwargs)
 
 def initialized(key, image_size):
   input_shape = (1, image_size, image_size, 3)
