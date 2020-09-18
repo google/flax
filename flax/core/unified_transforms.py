Fix scan broadcast issue
broadcast variables were re-computed during init in each iterations causing excessive slow down.

flax/core/unified_transforms.py
@@ -46,46 +46,49 @@ def scan(
     length: Optional[int] = None,
     reverse: bool = False):
 
-  def transpose_to_front(axis, xs):
-    if axis is broadcast:
+  def transpose_to_front(ax, xs):
+    if ax is broadcast:
       return ()
+    if ax == 0:
+      return xs
     def trans(x):
       perm = tuple(range(x.ndim))
-      perm = (axis,) + tuple(np.delete(perm, axis))
+      perm = (ax,) + tuple(np.delete(perm, ax))
       return jnp.transpose(x, perm)
     return jax.tree_map(trans, xs)
 
-  def transpose_from_front(axis, xs):
-    if axis is broadcast:
+  def transpose_from_front(ax, xs):
+    if ax is broadcast:
       return ()
+    if ax == 0:
+      return xs
     def trans(x):
-      if axis < 0:
-        ax = x.ndim - axis
+      if ax < 0:
+        pax = x.ndim - ax
       else:
-        ax = axis
-      assert ax < x.ndim
-      perm = tuple(range(1, ax + 1)) + (0,) + tuple(range(ax + 1, x.ndim))
+        pax = ax
+      assert pax < x.ndim
+      perm = tuple(range(1, pax + 1)) + (0,) + tuple(range(pax + 1, x.ndim))
       return jnp.transpose(x, perm)
     return jax.tree_map(trans, xs)
 
-  def scan_fn(init, *args):
+  def scan_fn(broadcast_in, init, *args):
     xs = jax.tree_multimap(transpose_to_front, in_axes, args)
 
     def body_fn(c, xs, init_mode=False):
       # inject constants
       xs = jax.tree_multimap(lambda ax, arg, x: (arg if ax is broadcast else x),
                              in_axes, args, xs)
-      c, ys = fn(c, *xs)
+      broadcast_out, c, ys = fn(broadcast_in, c, *xs)
       
       if init_mode:
         ys = jax.tree_multimap(lambda ax, y: (y if ax is broadcast else ()),
                                out_axes, ys)
-        return ys
+        return broadcast_out, ys
       else:
         ys = jax.tree_multimap(lambda ax, y: (() if ax is broadcast else y),
                                out_axes, ys)
         return c, ys
-      return c, ys
     broadcast_body = functools.partial(body_fn, init_mode=True)
 
     carry_pvals = jax.tree_map(
@@ -104,22 +107,12 @@ def scan(
       if pv is not None:
         raise ValueError('broadcasted variable has a data dependency on the scan body.')
       out_flat.append(const)
-    constants_out = jax.tree_unflatten(out_tree(), out_flat)
-
+    broadcast_in, constants_out = jax.tree_unflatten(out_tree(), out_flat)
+    
     c, ys = lax.scan(body_fn, init, xs, length=length, reverse=reverse)
     ys = jax.tree_multimap(transpose_from_front, out_axes, ys)
     ys = jax.tree_multimap(lambda ax, const, y: (const if ax is broadcast else y),
                            out_axes, constants_out, ys)
-    return c, ys
+    return broadcast_in, c, ys
 
   return scan_fn
-
-
-# def loop(c, x, y):
-#   print(c, x, y)
-#   return c + 1, (x * 2, y * 2)
-
-
-# f = scan(loop, in_axes=(broadcast, 1), out_axes=(broadcast, 1))
-# c, (xs, ys) = f(0, 1., jnp.arange(3)[None])
-# print(c, xs, ys)

tests/linen/linen_transforms_test.py
@@ -159,7 +159,7 @@ class TransformTest(absltest.TestCase):
       @nn.compact
       def __call__(self, c, xs):
         LSTM = nn.scan(nn.LSTMCell,
-                       variable_axes={'params': nn.broadcast},
+                       variable_broadcast='params',
                        split_rngs={'params': False})
         return LSTM(name="lstm_cell")(c, xs)
 
@@ -188,7 +188,7 @@ class TransformTest(absltest.TestCase):
   def test_scan_decorated(self):
     class SimpleScan(nn.Module):
       @partial(nn.scan,
-               variable_axes={'params': nn.broadcast},
+               variable_broadcast='params',
                split_rngs={'params': False})
       @nn.compact
       def __call__(self, c, xs):