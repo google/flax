81ce95f6358869fd79cfbf507694891330de620a
Bernd Bohnet <bohnetbd@google.com>
2020-11-09 11:08:43 +0100

    ported nlp_seq to linen

--- examples/nlp_seq/models.py
+++ examples/nlp_seq/models.py
@@ -14,55 +14,34 @@
 
 """Transformer-based langauge models."""
 
-from flax import nn
+from flax import linen as nn
+
+from typing import Callable, Any, Optional
+
+import jax
 import jax.numpy as jnp
 import numpy as np
 
-
-def shift_right(x, train=True):
-  """Shift the input to the right by padding on axis 1."""
-  if train:
-    pad_widths = [(0, 0)] * len(x.shape)
-    pad_widths[1] = (1, 0)  # Padding on axis=1
-    padded = jnp.pad(
-        x, pad_widths, mode='constant', constant_values=x.dtype.type(0))
-    return padded[:, :-1]
-  else:
-    # Do nothing in predict mode, as then the sequence length is 1.
-    return x
+from flax import struct
 
 
-class Embed(nn.Module):
-  """Embedding Module.
-
-  A parameterized function from integers [0, n) to d-dimensional vectors.
-  """
-
-  def apply(self,
-            inputs,
-            num_embeddings,
-            features,
-            mode='input',
-            emb_init=nn.initializers.normal(stddev=1.0)):
-    """Applies Embed module.
-
-    Args:
-      inputs: input data
-      num_embeddings: number of embedding
-      features: size of the embedding dimension
-      mode: either 'input' or 'output' -> to share input/output embedding
-      emb_init: embedding initializer
-
-    Returns:
-      output which is embedded input data
-    """
-    embedding = self.param('embedding', (num_embeddings, features), emb_init)
-    if mode == 'input':
-      if inputs.dtype not in [jnp.int32, jnp.int64, jnp.uint32, jnp.uint64]:
-        raise ValueError('Input type must be an integer or unsigned integer.')
-      return jnp.take(embedding, inputs, axis=0)
-    if mode == 'output':
-      return jnp.einsum('bld,vd->blv', inputs, embedding)
+@struct.dataclass
+class TransformerConfig:
+  """Global hyperparameters used to minimize obnoxious kwarg plumbing."""
+  vocab_size: int
+  output_vocab_size: int
+  dtype: Any = jnp.float32
+  emb_dim: int = 512
+  num_heads: int = 8
+  num_layers: int = 6
+  qkv_dim: int = 512
+  mlp_dim: int = 2048
+  max_len: int = 2048
+  dropout_rate: float = 0.1
+  attention_dropout_rate: float = 0.1
+  kernel_init: Callable = nn.initializers.xavier_uniform()
+  bias_init: Callable = nn.initializers.normal(stddev=1e-6)
+  posemb_init: Optional[Callable] = None
 
 
 def sinusoidal_init(max_len=2048):
@@ -90,111 +69,131 @@ def sinusoidal_init(max_len=2048):
 
   return init
 
-
 class AddPositionEmbs(nn.Module):
-  """Adds learned positional embeddings to the inputs."""
+  """Adds (optionally learned) positional embeddings to the inputs.
 
-  def apply(self,
-            inputs,
-            max_len=2048,
-            posemb_init=nn.initializers.normal(stddev=1.0)):
+  Args:
+    config: TransformerConfig dataclass containing hyperparameters.
+  """
+  config: TransformerConfig
+
+  @nn.compact
+  def __call__(self,
+               inputs,
+               inputs_positions=None):
     """Applies AddPositionEmbs module.
 
+    By default this layer uses a fixed sinusoidal embedding table. If a
+    learned position embedding is desired, pass an initializer to
+    posemb_init in the configuration.
+
     Args:
-      inputs: input data
-      max_len: maximum possible length for the input
-      posemb_init: positional embedding initializer
+      inputs: input data.
+      inputs_positions: input position indices for packed sequences.
 
     Returns:
       output: `(bs, timesteps, in_dim)`
     """
-    assert inputs.ndim == 3, ('Number of dimention should be 3, but it is: %d' %
-                              inputs.ndim)
+    cfg = self.config
+    # inputs.shape is (batch_size, seq_len, emb_dim)
+    assert inputs.ndim == 3, ('Number of dimensions should be 3,'
+                              ' but it is: %d' % inputs.ndim)
     length = inputs.shape[1]
-    pos_emb_shape = (1, max_len, inputs.shape[-1])
-    pos_embedding = self.param('pos_embedding', pos_emb_shape, posemb_init)
-    return inputs + pos_embedding[:, :length, :]
+    pos_emb_shape = (1, cfg.max_len, inputs.shape[-1])
+    if cfg.posemb_init is None:
+      # Use a fixed (non-learned) sinusoidal position embedding.
+      pos_embedding = sinusoidal_init(max_len=cfg.max_len)(
+          None, pos_emb_shape, None)
+    else:
+      pos_embedding = self.param('pos_embedding',
+                                 cfg.posemb_init,
+                                 pos_emb_shape)
+    pe = pos_embedding[:, :length, :]
+
+    if inputs_positions is None:
+      # normal unpacked case:
+      return inputs + pe
+    else:
+      # for packed data we need to use known position indices:
+      return inputs + jnp.take(pe[0], inputs_positions, axis=0)
 
 
 class MlpBlock(nn.Module):
-  """Transformer MLP block."""
+  """Transformer MLP / feed-forward block.
 
-  def apply(self,
-            inputs,
-            mlp_dim,
-            out_dim=None,
-            dropout_rate=0.1,
-            deterministic=False,
-            kernel_init=nn.initializers.xavier_uniform(),
-            bias_init=nn.initializers.normal(stddev=1e-6)):
+  Args:
+    config: TransformerConfig dataclass containing hyperparameters.
+    out_dim: optionally specify out dimension.
+  """
+  config: TransformerConfig
+  out_dim: Optional[int] = None
+
+  @nn.compact
+  def __call__(self, inputs, deterministic=True):
     """Applies Transformer MlpBlock module."""
-    actual_out_dim = inputs.shape[-1] if out_dim is None else out_dim
-    x = nn.Dense(inputs, mlp_dim, kernel_init=kernel_init, bias_init=bias_init)
-    x = nn.gelu(x)
-    x = nn.dropout(x, rate=dropout_rate, deterministic=deterministic)
-    output = nn.Dense(
-        x, actual_out_dim, kernel_init=kernel_init, bias_init=bias_init)
-    output = nn.dropout(output, rate=dropout_rate, deterministic=deterministic)
+    cfg = self.config
+    actual_out_dim = (inputs.shape[-1] if self.out_dim is None
+                      else self.out_dim)
+    x = nn.Dense(cfg.mlp_dim,
+                 dtype=cfg.dtype,
+                 kernel_init=cfg.kernel_init,
+                 bias_init=cfg.bias_init)(inputs)
+    x = nn.relu(x)
+    x = nn.Dropout(rate=cfg.dropout_rate)(x, deterministic=deterministic)
+    output = nn.Dense(actual_out_dim,
+                      dtype=cfg.dtype,
+                      kernel_init=cfg.kernel_init,
+                      bias_init=cfg.bias_init)(x)
+    output = nn.Dropout(rate=cfg.dropout_rate)(
+        output, deterministic=deterministic)
     return output
 
 
-class Transformer1DBlock(nn.Module):
-  """Transformer layer (https://openreview.net/forum?id=H1e5GJBtDr)."""
+class Encoder1DBlock(nn.Module):
+  """Transformer decoder layer.
 
-  def apply(self,
-            inputs,
-            qkv_dim,
-            mlp_dim,
-            num_heads,
-            causal_mask=False,
-            padding_mask=None,
-            dropout_rate=0.1,
-            attention_dropout_rate=0.1,
-            deterministic=False):
-    """Applies Transformer1DBlock module.
+  Args:
+    config: TransformerConfig dataclass containing hyperparameters.
+  """
+  config: TransformerConfig
+
+  @nn.compact
+  def __call__(self,
+               inputs,
+               encoder_mask=None,
+               deterministic=True):
+    """Applies Encoder1DBlock module.
 
     Args:
-      inputs: input data
-      qkv_dim: dimension of the query/key/value
-      mlp_dim: dimension of the mlp on top of attention block
-      num_heads: number of heads
-      causal_mask: bool, mask future or not
-      padding_mask: bool, mask padding tokens
-      dropout_rate: dropout rate
-      attention_dropout_rate: dropout rate for attention weights
-      deterministic: bool, deterministic or not (to apply dropout)
+      inputs: input data.
+      encoder_mask: encoder self-attention mask.
 
     Returns:
-      output after transformer block.
-
+      output after transformer encoder block.
     """
+    cfg = self.config
 
     # Attention block.
     assert inputs.ndim == 3
-    x = nn.LayerNorm(inputs)
+    x = nn.LayerNorm(dtype=cfg.dtype)(inputs)
     x = nn.SelfAttention(
-        x,
-        num_heads=num_heads,
-        qkv_features=qkv_dim,
-        attention_axis=(1,),
-        causal_mask=causal_mask,
-        padding_mask=padding_mask,
-        kernel_init=nn.initializers.xavier_uniform(),
-        bias_init=nn.initializers.normal(stddev=1e-6),
-        bias=False,
+        num_heads=cfg.num_heads,
+        dtype=cfg.dtype,
+        qkv_features=cfg.qkv_dim,
+        kernel_init=cfg.kernel_init,
+        bias_init=cfg.bias_init,
+        use_bias=False,
         broadcast_dropout=False,
-        dropout_rate=attention_dropout_rate,
-        deterministic=deterministic)
-    x = nn.dropout(x, rate=dropout_rate, deterministic=deterministic)
+        dropout_rate=cfg.attention_dropout_rate,
+        deterministic=deterministic)(x, encoder_mask)
+
+    x = nn.Dropout(rate=cfg.dropout_rate)(
+        x, deterministic=deterministic)
     x = x + inputs
 
     # MLP block.
-    y = nn.LayerNorm(x)
-    y = MlpBlock(
-        y,
-        mlp_dim=mlp_dim,
-        dropout_rate=dropout_rate,
-        deterministic=deterministic)
+    y = nn.LayerNorm(dtype=cfg.dtype)(x)
+    y = MlpBlock(config=cfg)(y, deterministic=deterministic)
 
     return x + y
 
@@ -202,34 +201,18 @@ class Transformer1DBlock(nn.Module):
 class Transformer(nn.Module):
   """Transformer Model for sequence tagging."""
 
-  def apply(self,
+  config: TransformerConfig
+
+  @nn.compact
+  def __call__(self,
             inputs,
-            vocab_size,
-            output_vocab_size,
-            emb_dim=512,
-            num_heads=8,
-            num_layers=6,
-            qkv_dim=512,
-            mlp_dim=2048,
-            max_len=2048,
-            train=True,
-            dropout_rate=0.3,
-            attention_dropout_rate=0.3):
+            train=False,
+            dropout_rate=0.3):
     """Applies Transformer model on the inputs.
 
     Args:
       inputs: input data
-      vocab_size: size of the input vocabulary
-      output_vocab_size: size of the output classes
-      emb_dim: dimension of embedding
-      num_heads: number of heads
-      num_layers: number of layers
-      qkv_dim: dimension of the query/key/value
-      mlp_dim: dimension of the mlp on top of attention block
-      max_len: maximum length.
       train: if it is training,
-      dropout_rate: dropout rate
-      attention_dropout_rate: dropout rate for attention weights
 
     Returns:
       output of a transformer decoder.
@@ -238,27 +221,17 @@ class Transformer(nn.Module):
     padding_mask = jnp.where(inputs > 0, 1, 0).astype(jnp.float32)[..., None]
     assert inputs.ndim == 2  # (batch, len)
 
+    cfg=self.config
+
     x = inputs.astype('int32')
-    x = Embed(x, num_embeddings=vocab_size, features=emb_dim, name='embed')
-    x = nn.dropout(x, rate=dropout_rate, deterministic=not train)
-    x = AddPositionEmbs(
-        x, max_len=max_len, posemb_init=sinusoidal_init(max_len=max_len))
-    for _ in range(num_layers):
-      x = Transformer1DBlock(
-          x,
-          qkv_dim=qkv_dim,
-          mlp_dim=mlp_dim,
-          num_heads=num_heads,
-          causal_mask=False,
-          padding_mask=padding_mask,
-          dropout_rate=dropout_rate,
-          attention_dropout_rate=attention_dropout_rate,
-          deterministic=not train,
-      )
-    x = nn.LayerNorm(x)
+    x = nn.Embed(num_embeddings=cfg.vocab_size, features=cfg.emb_dim, name='embed')(x)
+    x = nn.Dropout(rate=dropout_rate)(x, deterministic=not train)
+    x = AddPositionEmbs(cfg)(x)
+    for _ in range(cfg.num_layers):
+       x = Encoder1DBlock(cfg)(x, deterministic=not train)
+    x = nn.LayerNorm(dtype=cfg.dtype)(x)
     logits = nn.Dense(
-        x,
-        output_vocab_size,
+        cfg.output_vocab_size,
         kernel_init=nn.initializers.xavier_uniform(),
-        bias_init=nn.initializers.normal(stddev=1e-6))
+        bias_init=nn.initializers.normal(stddev=1e-6))(x)
     return logits
