692b960acb4ebfb062fb26e881dc2d6274cdba2d
jheek <heekjonathan@gmail.com>
2020-11-17 15:03:29 +0000

    Add pytest

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ tests_require = [
     "pytest",
     "pytest-cov",
     "pytest-xdist==1.34.0",  # upgrading to 2.0 broke tests, need to investigate
+    "pytype",
     "sentencepiece",  # WMT example.
     "svn",
     "tensorflow",
