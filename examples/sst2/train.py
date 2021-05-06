a3990ed15390b3172b095dff27d0414c8ce96479
Jasmijn Bastings <bastings@users.noreply.github.com>
2021-05-06 16:38:48 +0200

    Switch to Optax optimizer and use TrainState.

--- examples/sst2/train.py
+++ examples/sst2/train.py
@@ -13,23 +13,28 @@
 # limitations under the License.
 
 """Trains an SST2 text classifier."""
-import copy
-import functools
-from typing import Any, Callable, Dict, Iterable, Mapping, Optional, Sequence, Tuple, Union
+from typing import Any, Callable, Dict, Iterable, Optional, Sequence, Tuple, Union
 
 from absl import logging
-from flax import optim
+import flax
 from flax.metrics import tensorboard
+from flax.training import train_state
 import input_pipeline
 import jax
 import jax.numpy as jnp
 import ml_collections
 import models
 import numpy as np
+import optax
 import tensorflow as tf
 
 Array = jnp.ndarray
 Example = Dict[str, Array]
+
+
+class TrainState(train_state.TrainState):
+  model_state: Any
+  dynamic_scale: Optional[flax.optim.DynamicScale]
 
 
 @jax.vmap
@@ -42,23 +47,35 @@ def sigmoid_cross_entropy_with_logits(*, labels: Array, logits: Array) -> Array:
   return relu_logits - logits * labels + jnp.log1p(jnp.exp(neg_abs_logits))
 
 
-def get_initial_params_and_state(key, model):
+def get_initial_params_and_state(rng, model):
   """Returns randomly initialized parameters and a fresh model state."""
   token_ids = jnp.ones((2, 3), jnp.int32)
   lengths = jnp.ones((2,), dtype=jnp.int32)
-  variables = model.init(key, token_ids, lengths)
-  state, params = variables.pop('params')
-  return params, state
+  variables = model.init(rng, token_ids, lengths, deterministic=True)
+  model_state, params = variables.pop('params')
+
+  # Remove intermediates for training. Otherwise our model state will fill up
+  # with intermediate outputs (exported using self.sow() commands). This will
+  # cause model_state to have a new shape on each step, triggering a new trace.
+  if 'intermediates' in model_state:
+    model_state, _ = model_state.pop('intermediates')
+
+  return params, model_state
 
 
-def create_optimizer(params, learning_rate, beta, weight_decay):
-  """Returns a momentum optimizer."""
-  optimizer_def = optim.Momentum(
-      learning_rate=learning_rate,
-      beta=beta,
-      weight_decay=weight_decay)
-  optimizer = optimizer_def.create(params)
-  return optimizer
+def create_train_state(rng, config: ml_collections.ConfigDict, model):
+  """Create initial training state."""
+  params, model_state = get_initial_params_and_state(rng, model)
+  tx = optax.chain(
+      optax.sgd(learning_rate=config.learning_rate, momentum=config.momentum),
+      optax.additive_weight_decay(weight_decay=config.weight_decay))
+  state = TrainState.create(
+      apply_fn=model.apply,
+      params=params,
+      tx=tx,
+      model_state=model_state,
+      dynamic_scale=None)
+  return state
 
 
 def compute_metrics(*, labels: Array, logits: Array) -> Dict[str, Array]:
@@ -85,29 +102,26 @@ def model_from_config(config: ml_collections.ConfigDict):
       output_size=config.output_size,
       dropout_rate=config.dropout_rate,
       word_dropout_rate=config.word_dropout_rate,
-      unk_idx=config.unk_idx,
-      deterministic=config.deterministic)
+      unk_idx=config.unk_idx)
   return model
 
 
 def train_step(
-    config: Any,
-    optimizer: optim.Optimizer,
-    model_state: Mapping[str, Any],
+    state: TrainState,
     batch: Dict[str, Array],
     rngs: Dict[str, Any],
-) -> Tuple[optim.Optimizer, Dict[str, Any], Dict[str, Any]]:
+) -> Tuple[TrainState, Dict[str, Any]]:
   """Train for a single step."""
   # Make sure to get a new RNG at every step.
-  model = model_from_config(config)
-  step = optimizer.state.step
+  step = state.step
   rngs = {name: jax.random.fold_in(rng, step) for name, rng in rngs.items()}
 
   def loss_fn(params):
-    variables = {'params': params, **model_state}
-    logits, new_model_state = model.apply(
+    variables = {'params': params, **state.model_state}
+    logits, new_model_state = state.apply_fn(
         variables, batch['token_ids'], batch['length'],
-        rngs=rngs, mutable=list(model_state.keys()))
+        deterministic=False,
+        rngs=rngs, mutable=list(state.model_state.keys()))
 
     labels = batch['label']
     if labels.ndim == 1:
@@ -117,26 +131,25 @@ def train_step(
     return loss, (logits, new_model_state)
 
   grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
-  value, grad = grad_fn(optimizer.target)
+  value, grads = grad_fn(state.params)
   (_, (logits, new_model_state)) = value
-  optimizer = optimizer.apply_gradient(grad)
 
+  new_state = state.apply_gradients(grads=grads, model_state=new_model_state)
   metrics = compute_metrics(labels=batch['label'], logits=logits)
-  return optimizer, metrics, new_model_state
+  return new_state, metrics
 
 
-def eval_step(config: Any, params: Dict[str, Any],
-              model_state: Mapping[str, Any], batch: Dict[str, Array],
-              rngs: Dict[str, Any]) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+def eval_step(state: TrainState, batch: Dict[str, Array],
+              rngs: Dict[str, Any]) -> Dict[str, Any]:
   """Evaluate for a single step. Model should be in deterministic mode."""
-  model = model_from_config(config)
-  variables = {'params': params, **model_state}
-  logits, new_model_state = model.apply(
+  variables = {'params': state.params, **state.model_state}
+  logits, _ = state.apply_fn(
       variables, batch['token_ids'], batch['length'],
+      deterministic=True,
       rngs=rngs,
-      mutable=list(model_state.keys()))
+      mutable=list(state.model_state.keys()))
   metrics = compute_metrics(labels=batch['label'], logits=logits)
-  return metrics, new_model_state
+  return metrics
 
 
 def normalize_batch_metrics(
@@ -154,13 +167,12 @@ def normalize_batch_metrics(
 
 
 def evaluate_model(
-        eval_step_fn: Callable[..., Tuple[Dict[str, Any], Dict[str, Any]]],
-        params: Dict[str, Any],
-        model_state: Mapping[str, Any],
+        eval_step_fn: Callable[..., Any],
+        state: TrainState,
         batches: Union[Iterable[Example], tf.data.Dataset],
         epoch: int,
         rngs: Optional[Dict[str, Any]] = None
-) -> Tuple[Dict[str, Any], Mapping[str, Any]]:
+) -> Dict[str, Any]:
   """Evaluate a model on a dataset."""
   batch_metrics = []
   for i, batch in enumerate(batches):
@@ -168,27 +180,26 @@ def evaluate_model(
     if rngs is not None:  # New RNG for each step.
       rngs = {name: jax.random.fold_in(rng, i) for name, rng in rngs.items()}
 
-    metrics, model_state = eval_step_fn(params, model_state, batch, rngs)
+    metrics = eval_step_fn(state, batch, rngs)
     batch_metrics.append(metrics)
 
   batch_metrics = jax.device_get(batch_metrics)
   metrics = normalize_batch_metrics(batch_metrics)
   logging.info('eval  epoch %03d loss %.4f accuracy %.2f', epoch,
                metrics['loss'], metrics['accuracy'] * 100)
-  return metrics, model_state
+  return metrics
 
 
-def train_epoch(train_step_fn: Callable[..., Tuple[optim.Optimizer,
-                                                   Dict[str, Any], Any]],
-                optimizer: optim.Optimizer,
-                model_state: Mapping[str, Any], train_batches: tf.data.Dataset,
-                epoch: int, rngs: Optional[Dict[str, Any]] = None):
+def train_epoch(train_step_fn: Callable[..., Tuple[TrainState, Dict[str, Any]]],
+                state: TrainState,
+                train_batches: tf.data.Dataset,
+                epoch: int,
+                rngs: Optional[Dict[str, Any]] = None):
   """Train for a single epoch."""
   batch_metrics = []
   for batch in train_batches:
     batch = jax.tree_map(lambda x: x._numpy(), batch)  # pylint: disable=protected-access
-    optimizer, metrics, model_state = train_step_fn(
-        optimizer, model_state, batch, rngs)
+    state, metrics = train_step_fn(state, batch, rngs)
     batch_metrics.append(metrics)
 
   # Compute the metrics for this epoch.
@@ -198,18 +209,18 @@ def train_epoch(train_step_fn: Callable[..., Tuple[optim.Optimizer,
   logging.info('train epoch %03d loss %.4f accuracy %.2f', epoch,
                metrics['loss'], metrics['accuracy'] * 100)
 
-  return optimizer, metrics, model_state
+  return state, metrics
 
 
 def train_and_evaluate(config: ml_collections.ConfigDict,
-                       workdir: str) -> optim.Optimizer:
+                       workdir: str) -> TrainState:
   """Execute model training and evaluation loop.
 
   Args:
     config: Hyperparameter configuration for training and evaluation.
     workdir: Directory where the tensorboard summaries are written to.
   Returns:
-    The trained optimizer.
+    The final train state that includes the trained parameters.
   """
   # Prepare datasets.
   train_dataset = input_pipeline.TextDataset(
@@ -227,26 +238,15 @@ def train_and_evaluate(config: ml_collections.ConfigDict,
 
   # Prepare configs.
   config.vocab_size = len(train_dataset.vocab)
-  eval_config = copy.deepcopy(config)
-  eval_config.deterministic = True
 
   # Compile step functions.
-  train_step_fn = jax.jit(functools.partial(train_step, config))
-  eval_step_fn = jax.jit(functools.partial(eval_step, eval_config))
+  train_step_fn = jax.jit(train_step)
+  eval_step_fn = jax.jit(eval_step)
 
-  # Initialize parameters.
+  # Create model and a state that contains the parameters.
   rng = jax.random.PRNGKey(config.seed)
-  init_model = model_from_config(eval_config)
-  params, model_state = get_initial_params_and_state(rng, init_model)
-  del init_model
-  
-  # Remove intermediates for training. Otherwise our model state will fill up
-  # with intermediate outputs (exported using self.sow() commands). This will
-  # cause model_state to have a new shape on each step, triggering a new trace.
-  model_state, _ = model_state.pop('intermediates')  
-  
-  optimizer = create_optimizer(
-      params, config.learning_rate, config.momentum, config.weight_decay)
+  model = model_from_config(config)
+  state = create_train_state(rng, config, model)
 
   summary_writer = tensorboard.SummaryWriter(workdir)
   summary_writer.hparams(dict(config))
@@ -258,12 +258,11 @@ def train_and_evaluate(config: ml_collections.ConfigDict,
     # Train for one epoch.
     rng, epoch_rng = jax.random.split(rng)
     rngs = {'dropout': epoch_rng}
-    optimizer, train_metrics, model_state = train_epoch(
-        train_step_fn, optimizer, model_state, train_batches, epoch, rngs)
+    state, train_metrics = train_epoch(
+        train_step_fn, state, train_batches, epoch, rngs)
 
     # Evaluate current model on the validation data.
-    eval_metrics, _ = evaluate_model(
-        eval_step_fn, optimizer.target, model_state, eval_batches, epoch)
+    eval_metrics = evaluate_model(eval_step_fn, state, eval_batches, epoch)
 
     # Write metrics to TensorBoard.
     summary_writer.scalar('train_loss', train_metrics['loss'], epoch)
@@ -278,4 +277,4 @@ def train_and_evaluate(config: ml_collections.ConfigDict,
         epoch)
 
   summary_writer.flush()
-  return optimizer
+  return state
