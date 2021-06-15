0f4947f25c04925d181eaacd68b6ebabd55c3850
andsteing <andstein@google.com>
2021-06-15 16:29:31 +0200

    Update flax/optim/__init__.py
    Co-authored-by: Marc van Zee <marcvanzee@gmail.com>

--- flax/optim/__init__.py
+++ flax/optim/__init__.py
@@ -15,7 +15,7 @@
 """Flax Optimizer api.
 
 Note that with `FLIP #1009`_ the optimizers in ``flax.optim`` were **effectively
-deprecated** in favor of Optax_ optimizers. There is not feature parity yet (e.g.
+deprecated** in favor of Optax_ optimizers. There is no feature parity yet (e.g.
 ``AdaFactor`` is missing in Optax), but the large majority of use cases is well
 supported, and Optax is actively being developed and continuously adds new
 features and optimizers.
