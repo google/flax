5ac7dc83102337808350abcc31f969e478d098a8
mattiasmar <mattias.marder@intel.com>
2022-01-23 23:25:54 +0200

    Update download_dataset_metadata.sh
    Use git sparse operation instead of svn.

--- tests/download_dataset_metadata.sh
+++ tests/download_dataset_metadata.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
+
 set -e
 
-# Download TFDS metadata to flax/.tdfs/metadata directory. 
+# Download TFDS metadata to flax/.tdfs/metadata directory.
 # This allows the tests to specify the `data_dir` when using tfds.testing.mock_data().
 cd "$( dirname "$0" )"
 
-if [ -d "../.tfds/metadata" ]; then 
-  echo 'TFDS metadata already exists.'; 
-else 
+if [ -d "../.tfds/metadata" ]; then
+  echo 'TFDS metadata already exists.';
+else
   echo 'TFDS metadata does not exist. Downloading...';
-  # Note: We use subversions for faster downloads of only the metadata subdirectory
-  # with no history. If this script fails for you because you don't have subversion 
-  # installed, please let us know by opening a GitHub issue.
-  # 
-  # subversion checkout to the `trunk` branch which corresponds to `tree/master`.
-  # To download from branch `foo`, replace `trunk` with `branches/foo`.
-  svn checkout \
-    https://github.com/tensorflow/datasets/trunk/tensorflow_datasets/testing/metadata \
-    "../.tfds/metadata" -q; 
+  git clone --depth 3  --filter=blob:none   --sparse   https://github.com/tensorflow/datasets/
+  cd datasets
+  git sparse-checkout set tensorflow_datasets/testing/metadata
+  mkdir ../../.tfds
+  mv tensorflow_datasets/testing/metadata/ ../../.tfds/metadata/
+  cd ..
+  rm datasets -rf
 fi
