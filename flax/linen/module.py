e85344ac66af36bfdc8a4b49245b6f2daf188b36
Neil Girdhar <mistersheik@gmail.com>
2022-02-10 00:17:47 -0500

    Fix bug with generic module class instantiation.

--- flax/linen/module.py
+++ flax/linen/module.py
@@ -662,19 +662,32 @@ class Module(metaclass=ModuleMeta):
       name: Attribute to set.
       val: Value of the attribute.
     """
-    is_dataclass_attr = name in self.__dataclass_fields__ and self.__dataclass_fields__[name].init  # pytype: disable=attribute-error
+    is_dataclass_attr = (
+      name in self.__dataclass_fields__ and  # pytype: disable=attribute-error
+      self.__dataclass_fields__[name].init)  # pytype: disable=attribute-error
 
     if not self._state.in_setup and self._state.is_initialized:
       # Raises a TypeError just like frozen python dataclasses.
       raise errors.SetAttributeFrozenModuleError(self.__class__.__name__, name,
                                                  val)
     if is_dataclass_attr:
+      # These names are specified as dataclass fields.  They should not be
+      # initialized within the setup() method, but can be modified freely
+      # outside it.
       if self._state.in_setup:
         raise errors.SetAttributeInModuleSetupError()
       object.__setattr__(self, name, val)
+      return
+
+    if name.startswith('__') and name.endswith('__'):
+      # "Magic" values are usually none of Flax's business.  One example is
+      # __orig_class__, which is written by CPython's _BaseGenericAlias to
+      # implement generic class instantiation with specified generic types.
+      object.__setattr__(self, name, val)
+      return
+
     # Submodules are being defined and attached in setup()
-    else:
-      self._register_submodules(name, val)
+    self._register_submodules(name, val)
 
   def __getattr__(self, name: str) -> Any:
     """Call setup() before getting any setup-defined attributes."""
