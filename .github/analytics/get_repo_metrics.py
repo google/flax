44e3c661b71ad9f0d467c275229fcd7d50eb8e71
Cristian Garcia <cgarcia.e88@gmail.com>
2022-05-16 15:35:25 -0500

    adapt style

--- .github/analytics/get_repo_metrics.py
+++ .github/analytics/get_repo_metrics.py
@@ -14,46 +14,25 @@ token = os.environ["GITHUB_TOKEN"]
 endpoint = r"https://api.github.com/graphql"
 headers = {"Authorization": f"bearer {token}"}
 
+#------------------------------------------------------------------------------
+# GraphQL
+#------------------------------------------------------------------------------
+# NOTE: This GraphQL logic was ported and adapted from this script:
+# https://github.com/scientific-python/devstats-data/blob/4c022961abc4ca6061f8719d9c3387e98734b90c/query.py
+# It contains style differences from Google's style guide.
 
-def load_query_from_file(fname, repo_owner, repo_name):
-  """
-  Load an 'issue' query from file and set the target repository, where
-  the target repository has the format:
-  
-  https://github.com/<repo_owner>/<repo_name>
-  
-  Parameters
-  ----------
-  fname : str
-    Path to a text file containing a valid issue query according to the 
-    GitHub GraphQL schema.
-  repo_owner : str
-    Owner of target repository on GitHub. Default is 'numpy'.
-  repo_name : str
-    Name of target repository on GitHub. Default is 'numpy'.
-  
-  Returns
-  -------
-  query : str
-    Query loaded from file in text form suitable for ``send_query``.
-
-  Notes
-  -----
-  This function expects the query to have a specific form and will not work
-  for general GitHub GraphQL queries. See ``examples/`` for some valid
-  templated issue queries.
-  """
-  with open(fname, "r") as fh:
+def load_query_from_file(fname, repo_owner, repo_name) -> str:
+  with open(fname) as fh:
     query = fh.read()
     # Set target repo from template
-    query = query.replace("_REPO_OWNER_", repo_owner)
-    query = query.replace("_REPO_NAME_", repo_name)
+    query = query.replace('_REPO_OWNER_', repo_owner)
+    query = query.replace('_REPO_NAME_', repo_name)
   return query
 
 
 def send_query(query, query_type, cursor=None):
   """
-  Send a GraphQL query via requests.post
+  Sends a GraphQL to the GitHub API.
 
   No validation is done on the query before sending. GitHub GraphQL is
   supported with the `cursor` argument.
@@ -80,15 +59,15 @@ def send_query(query, query_type, cursor=None):
   """
   # TODO: Expand this, either by parsing the query type from the query
   # directly or manually adding more query_types to the set
-  if query_type not in {"issues", "pullRequests"}:
+  if query_type not in {'issues', 'pullRequests'}:
       raise ValueError(
-          "Only 'issues' and 'pullRequests' queries are currently supported"
+          'Only \'issues\' and \'pullRequests\' queries are currently supported'
       )
   # TODO: Generalize this
   # WARNING: The cursor injection depends on the specific structure of the
   # query, this is the main reason why query types are limited to issues/PRs
   if cursor is not None:
-    cursor_insertion_key = query_type + "("
+    cursor_insertion_key = query_type + '('
     cursor_ind = query.find(cursor_insertion_key) + len(cursor_insertion_key)
     query = query[:cursor_ind] + f'after:"{cursor}", ' + query[cursor_ind:]
   # Build request payload
@@ -97,25 +76,23 @@ def send_query(query, query_type, cursor=None):
   return json.loads(response.content)
 
 def get_all_responses(query, query_type):
-  """
-  Helper function to bypass GitHub GraphQL API node limit.
-  """
+  "Helper function to bypass GitHub GraphQL API node limit."
   # Get data from a single response
   initial_data = send_query(query, query_type)
   data, last_cursor, total_count = parse_single_query(initial_data, query_type)
-  print(f"Retrieving {len(data)} out of {total_count} values...")
+  print(f'Retrieving {len(data)} out of {total_count} values...')
   # Continue requesting data (with pagination) until all are acquired
   while len(data) < total_count:
     rdata = send_query(query, query_type, cursor=last_cursor)
     pdata, last_cursor, _ = parse_single_query(rdata, query_type)
     data.extend(pdata)
-    print(f"Retrieving {len(data)} out of {total_count} values...")
-  print("Done.")
+    print(f'Retrieving {len(data)} out of {total_count} values...')
+  print('Done.')
   return data
 
 def parse_single_query(data, query_type):
   """
-  Parse the data returned by `send_query`
+  Parses the data returned by `send_query`
 
   .. warning::
       
@@ -135,7 +112,7 @@ def parse_single_query(data, query_type):
 
 class GithubGrabber:
   """
-  Pull down data via the GitHub APIv.4 given a valid GraphQL query.
+  Pulls down data via the GitHub APIv.4 given a valid GraphQL query.
   """
 
   def __init__(self, query_fname, query_type, repo_owner, repo_name):
@@ -155,9 +132,9 @@ class GithubGrabber:
       Type of object that is being queried according to the GitHub GraphQL
       schema. Currently only "issues" and "pullRequests" are supported.
     repo_owner : str
-      Repository owner. Default is "numpy"
+      Repository owner.
     repo_name : str
-      Repository name. Default is "numpy"
+      Repository name.
     """
     self.query_fname = query_fname
     self.query_type = query_type  # TODO: Parse this directly from query
@@ -172,47 +149,37 @@ class GithubGrabber:
     )
 
   def get(self):
-    """
-    Get JSON-formatted raw data from the query.
-    """
     self.raw_data = get_all_responses(self.query, self.query_type)
 
-  def dump(self, outfile):
-    """
-    Dump raw json to `outfile`.
-    """
-    if not self.raw_data:
-        raise ValueError("raw_data is currently empty, nothing to dump")
-
-    with open(outfile, "w") as outf:
-      json.dump(self.raw_data, outf)
-
+#------------------------------------------------------------------------------
+# metrics helpers
+#------------------------------------------------------------------------------
 
 def _to_datetime(date_str: str) -> datetime:
-  return datetime.fromisoformat(date_str.replace("Z", ""))
+  return datetime.fromisoformat(date_str.replace('Z', ''))
 
 def _get_issues_features(issues):
   for issue in issues:
-    issue = issue["node"]
+    issue = issue['node']
 
-    created_at = _to_datetime(issue["createdAt"])
+    created_at = _to_datetime(issue['createdAt'])
     time_labeled_or_converted = None
     time_issue_closed = None
 
-    for event in issue["timelineItems"]["edges"]:
-      event = event["node"]
+    for event in issue['timelineItems']['edges']:
+      event = event['node']
 
-      if event["__typename"] in {"LabeledEvent", "ConvertedToDiscussionEvent"}:
-        time_labeled_or_converted = _to_datetime(event["createdAt"])
+      if event['__typename'] in {'LabeledEvent', 'ConvertedToDiscussionEvent'}:
+        time_labeled_or_converted = _to_datetime(event['createdAt'])
 
-      if event["__typename"] == "ClosedEvent":
-        time_issue_closed = _to_datetime(event["createdAt"])
+      if event['__typename'] == 'ClosedEvent':
+        time_issue_closed = _to_datetime(event['createdAt'])
 
     yield {
-      "created_at": created_at,
-      "time_labeled_or_converted": time_labeled_or_converted,
-      "time_issue_closed": time_issue_closed,
-      "issue_closed": issue["state"] == "CLOSED",
+      'created_at': created_at,
+      'time_labeled_or_converted': time_labeled_or_converted,
+      'time_issue_closed': time_issue_closed,
+      'issue_closed': issue['state'] == 'CLOSED',
     }
 
 def _start_of_month(date: datetime) -> datetime:
@@ -237,30 +204,30 @@ def _rolling_window(
   step: int = 1,
 ) -> pd.DataFrame:
   # start of month of the first issue
-  start: datetime = df.iloc[0]["created_at"].replace(
+  start: datetime = df.iloc[0]['created_at'].replace(
     day=1, hour=0, minute=0, second=0, microsecond=0
   )
   end = _shift_n_months(start, window_size)
 
-  last_month = _start_of_month(df.iloc[-1]["created_at"])
+  last_month = _start_of_month(df.iloc[-1]['created_at'])
   last_month = _shift_n_months(last_month, 1)
 
   rows: List[pd.Series] = []
   while end < last_month:
-    row = f(df[(df["created_at"] >= start) & (df["created_at"] < end)])
-    row["period_start"] = start
-    row["period_end"] = end
+    row = f(df[(df['created_at'] >= start) & (df['created_at'] < end)])
+    row['period_start'] = start
+    row['period_end'] = end
     rows.append(row)
     start = _shift_n_months(start, step)
     end = _shift_n_months(end, step)
 
   df = pd.DataFrame(rows)
-  df = df[["period_start", "period_end"] + list(df.columns[:-2])]
+  df = df[['period_start', 'period_end'] + list(df.columns[:-2])]
 
   return df
 
 def _process_issues(df: pd.DataFrame) -> pd.Series:
-  series: pd.Series = df[["issue_response_time", "issue_resolution_time"]].mean().dt.days
+  series: pd.Series = df[['issue_response_time', 'issue_resolution_time']].mean().dt.days
   series = series.copy()
   # series["num_open_issues"] = (~df["issue_closed"]).sum()
   # series["num_closed_issues"] = df["issue_closed"].sum()
@@ -283,8 +250,8 @@ def main(
   issues.get()
 
   df_issues = pd.DataFrame(list(_get_issues_features(issues.raw_data)))
-  df_issues["issue_response_time"] = df_issues["time_labeled_or_converted"] - df_issues["created_at"]
-  df_issues["issue_resolution_time"] = df_issues["time_issue_closed"] - df_issues["created_at"]
+  df_issues['issue_response_time'] = df_issues['time_labeled_or_converted'] - df_issues['created_at']
+  df_issues['issue_resolution_time'] = df_issues['time_issue_closed'] - df_issues['created_at']
 
   df_issues = _rolling_window(df_issues, _process_issues)
 
@@ -299,20 +266,20 @@ def main(
 
   # plot for isssue_response_time
   plt.figure()
-  plt.plot(df_issues["period_end"], df_issues["issue_response_time"])
-  plt.xlabel("Date")
-  plt.ylabel("Issue Response Time (days)")
-  plt.title("Issue Response Time")
+  plt.plot(df_issues['period_end'], df_issues['issue_response_time'])
+  plt.xlabel('Date')
+  plt.ylabel('Issue Response Time (days)')
+  plt.title('Issue Response Time')
   plt.gca().xaxis.set_major_locator(plt.MaxNLocator(5))
   plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
   plt.ylim(0)
 
   # plot for issue_resolution_time
   plt.figure()
-  plt.plot(df_issues["period_end"], df_issues["issue_resolution_time"])
-  plt.xlabel("Date")
-  plt.ylabel("Issue Resolution Time (days)")
-  plt.title("Issue Resolution Time")
+  plt.plot(df_issues['period_end'], df_issues['issue_resolution_time'])
+  plt.xlabel('Date')
+  plt.ylabel('Issue Resolution Time (days)')
+  plt.title('Issue Resolution Time')
   plt.gca().xaxis.set_major_locator(plt.MaxNLocator(5))
   plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
   plt.ylim(0)
@@ -320,5 +287,5 @@ def main(
   # show plots
   plt.show()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   main()
