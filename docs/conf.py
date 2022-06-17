5c2517ab740f640f0d5b43a324c0f502781dc6d9
Filippo Vicentini <filippovicentini@gmail.com>
2022-06-17 11:03:05 +0200

    Show repository button, launch in colab button

--- docs/conf.py
+++ docs/conf.py
@@ -98,6 +98,15 @@ html_logo = './flax.png'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_theme_options = {
+    "repository_url": "https://github.com/google/flax",
+    "use_repository_button": True,     # add a "link to repository" button
+    "use_issues_button": True,         # add an "Open an Issue" button
+    "path_to_docs": "docs",            # used to compute the path to launch notebooks in colab
+    "launch_buttons": {"colab_url": "https://colab.research.google.com"},
+}
+
+
 nbsphinx_codecell_lexer = 'ipython3'
 
 nbsphinx_prolog = r"""
