72189153f9779022b97858ae747c23fbaf571e3d
Lucas Yunkyu Lee <me@luc.li>
2022-08-18 09:08:13 +0900

    Ease rich version requirement

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ install_requires = [
     "matplotlib",  # only needed for tensorboard export
     "msgpack",
     "optax",
-    "rich~=11.1",
+    "rich>=11.1",
     "typing_extensions>=4.1.1",
     "PyYAML>=5.4.1",
 ]
