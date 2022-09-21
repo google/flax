d4fa16d3ab89038f1d4dd1e7ea552afc5a987285
idongheon <gnova6kud@gmail.com>
2022-09-21 22:24:10 +0900

    Add test_avg_pool_no_batch under test.linen.linen_test.PoolTest

--- tests/linen/linen_test.py
+++ tests/linen/linen_test.py
@@ -34,60 +34,86 @@ jax.config.parse_flags_with_absl()
 
 class PoolTest(parameterized.TestCase):
 
-    def test_pool_custom_reduce(self):
-        x = jnp.full((1, 3, 3, 1), 2.)
-        mul_reduce = lambda x, y: x * y
-        y = nn.pooling.pool(x, 1., mul_reduce, (2, 2), (1, 1), 'VALID')
-        np.testing.assert_allclose(y, np.full((1, 2, 2, 1), 2. ** 4))
+  def test_pool_custom_reduce(self):
+    x = jnp.full((1, 3, 3, 1), 2.)
+    mul_reduce = lambda x, y: x * y
+    y = nn.pooling.pool(x, 1., mul_reduce, (2, 2), (1, 1), 'VALID')
+    np.testing.assert_allclose(y, np.full((1, 2, 2, 1), 2. ** 4))
 
-    @parameterized.parameters(
-        {'count_include_pad': True},
-        {'count_include_pad': False})
-    def test_avg_pool(self, count_include_pad):
-        x = jnp.full((1, 3, 3, 1), 2.)
-        pool = lambda x: nn.avg_pool(x, (2, 2), count_include_pad=count_include_pad)
-        y = pool(x)
-        np.testing.assert_allclose(y, np.full((1, 2, 2, 1), 2.))
-        y_grad = jax.grad(lambda x: pool(x).sum())(x)
-        expected_grad = jnp.array([
-            [0.25, 0.5, 0.25],
-            [0.5, 1., 0.5],
-            [0.25, 0.5, 0.25],
-        ]).reshape((1, 3, 3, 1))
-        np.testing.assert_allclose(y_grad, expected_grad)
+  @parameterized.parameters(
+      {'count_include_pad': True},
+      {'count_include_pad': False})
+  def test_avg_pool(self, count_include_pad):
+    x = jnp.full((1, 3, 3, 1), 2.)
+    pool = lambda x: nn.avg_pool(x, (2, 2), count_include_pad=count_include_pad)
+    y = pool(x)
+    np.testing.assert_allclose(y, np.full((1, 2, 2, 1), 2.))
+    y_grad = jax.grad(lambda x: pool(x).sum())(x)
+    expected_grad = jnp.array([
+        [0.25, 0.5, 0.25],
+        [0.5, 1., 0.5],
+        [0.25, 0.5, 0.25],
+    ]).reshape((1, 3, 3, 1))
+    np.testing.assert_allclose(y_grad, expected_grad)
 
-    @parameterized.parameters(
-        {'count_include_pad': True},
-        {'count_include_pad': False})
-    def test_avg_pool_no_batch(self, count_include_pad):
-        x = jnp.full((3, 3, 1), 2.)
-        pool = lambda x: nn.avg_pool(x, (2, 2), count_include_pad=count_include_pad)
-        y = pool(x)
-        np.testing.assert_allclose(y, np.full((2, 2, 1), 2.))
-        y_grad = jax.grad(lambda x: pool(x).sum())(x)
-        expected_grad = jnp.array([
-            [0.25, 0.5, 0.25],
-            [0.5, 1., 0.5],
-            [0.25, 0.5, 0.25],
-        ]).reshape((3, 3, 1))
-        np.testing.assert_allclose(y_grad, expected_grad)
+  @parameterized.parameters(
+      {'count_include_pad': True},
+      {'count_include_pad': False})
+  def test_avg_pool_no_batch(self, count_include_pad):
+    x = jnp.full((3, 3, 1), 2.)
+    pool = lambda x: nn.avg_pool(x, (2, 2), count_include_pad=count_include_pad)
+    y = pool(x)
+    np.testing.assert_allclose(y, np.full((2, 2, 1), 2.))
+    y_grad = jax.grad(lambda x: pool(x).sum())(x)
+    expected_grad = jnp.array([
+        [0.25, 0.5, 0.25],
+        [0.5, 1., 0.5],
+        [0.25, 0.5, 0.25],
+    ]).reshape((3, 3, 1))
+    np.testing.assert_allclose(y_grad, expected_grad)
 
-    def test_max_pool(self):
-        x = jnp.arange(9).reshape((1, 3, 3, 1)).astype(jnp.float32)
-        pool = lambda x: nn.max_pool(x, (2, 2))
-        expected_y = jnp.array([
-            [4., 5.],
-            [7., 8.],
-        ]).reshape((1, 2, 2, 1))
-        y = pool(x)
-        np.testing.assert_allclose(y, expected_y)
-        y_grad = jax.grad(lambda x: pool(x).sum())(x)
-        expected_grad = jnp.array([
-            [0., 0., 0.],
-            [0., 1., 1.],
-            [0., 1., 1.],
-        ]).reshape((1, 3, 3, 1))
-        np.testing.assert_allclose(y_grad, expected_grad)
+  @parameterized.parameters(
+      {'count_include_pad': True},
+      {'count_include_pad': False})
+  def test_avg_pool_padding_same(self, count_include_pad):
+    x = jnp.array([1.0, 2.0, 3.0, 4.0]).reshape((1, 2, 2, 1))
+    pool = lambda x: nn.avg_pool(x, (2, 2), padding="SAME", count_include_pad=count_include_pad)
+    y = pool(x)
+    if count_include_pad:
+        expected_y = jnp.array([10.0 / 4, 6.0 / 4, 7.0 / 4, 4.0 / 4]).reshape((1, 2, 2, 1))
+    else:
+        expected_y = jnp.array([10.0 / 4, 6.0 / 2, 7.0 / 2, 4.0 / 1]).reshape((1, 2, 2, 1))
+    np.testing.assert_allclose(y, expected_y)
+
+  def test_max_pool(self):
+    x = jnp.arange(9).reshape((1, 3, 3, 1)).astype(jnp.float32)
+    pool = lambda x: nn.max_pool(x, (2, 2))
+    expected_y = jnp.array([
+        [4., 5.],
+        [7., 8.],
+    ]).reshape((1, 2, 2, 1))
+    y = pool(x)
+    np.testing.assert_allclose(y, expected_y)
+    y_grad = jax.grad(lambda x: pool(x).sum())(x)
+    expected_grad = jnp.array([
+        [0., 0., 0.],
+        [0., 1., 1.],
+        [0., 1., 1.],
+    ]).reshape((1, 3, 3, 1))
+    np.testing.assert_allclose(y_grad, expected_grad)
+
+  @parameterized.parameters(
+      {'count_include_pad': True},
+      {'count_include_pad': False})
+  def test_avg_pool_padding_same(self, count_include_pad):
+    x = jnp.array([1.0, 2.0, 3.0, 4.0]).reshape((1, 2, 2, 1))
+    pool = lambda x: nn.avg_pool(x, (2, 2), padding="SAME", count_include_pad=count_include_pad)
+    y = pool(x)
+    if count_include_pad:
+        expected_y = jnp.array([10.0 / 4, 6.0 / 4, 7.0 / 4, 4.0 / 4]).reshape((1, 2, 2, 1))
+    else:
+        expected_y = jnp.array([10.0 / 4, 6.0 / 2, 7.0 / 2, 4.0 / 1]).reshape((1, 2, 2, 1))
+    np.testing.assert_allclose(y, expected_y)
 
 
 class NormalizationTest(parameterized.TestCase):
