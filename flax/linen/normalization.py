Fix norm calculation bug for 0-rank arrays.
PiperOrigin-RevId: 548228171

flax/linen/normalization.py
@@ -97,7 +97,10 @@ def _compute_stats(
     if use_fast_variance:
       mean = x.mean(axes)
       mean2 = _abs_sq(x).mean(axes)
-      mean, mean2 = jnp.split(pmean(jnp.concatenate([mean, mean2])), 2)
+      if mean.ndim > 0:
+        mean, mean2 = jnp.split(pmean(jnp.concatenate([mean, mean2])), 2)
+      else:
+        mean, mean2 = pmean(mean), pmean(mean2)
       # mean2 - _abs_sq(mean) is not guaranteed to be non-negative due
       # to floating point round-off errors.
       var = jnp.maximum(0.0, mean2 - _abs_sq(mean))