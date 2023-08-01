9b0b4efda8320bd25b5f1403181e7b24589d84f4
Flax Team <no-reply@google.com>
2023-08-01 02:51:51 -0700

    Use field_specifiers instead of field_descriptors in @dataclass_transform.
    The parameter field_descriptor is an old naming in the initial draft of https://peps.python.org/pep-0681.
    The latest version and the official docs use field_specifiers instead.
    See https://docs.python.org/3/library/typing.html#typing.dataclass_transform.
    
    PiperOrigin-RevId: 552743767

--- flax/struct.py
+++ flax/struct.py
@@ -30,7 +30,7 @@ def field(pytree_node=True, **kwargs):
   return dataclasses.field(metadata={'pytree_node': pytree_node}, **kwargs)
 
 
-@dataclass_transform(field_descriptors=(field,))  # type: ignore[literal-required]
+@dataclass_transform(field_specifiers=(field,))  # type: ignore[literal-required]
 def dataclass(clz: _T) -> _T:
   """Create a class which can be passed to functional transformations.
 
@@ -181,7 +181,7 @@ def dataclass(clz: _T) -> _T:
 TNode = TypeVar('TNode', bound='PyTreeNode')
 
 
-@dataclass_transform(field_descriptors=(field,))  # type: ignore[literal-required]
+@dataclass_transform(field_specifiers=(field,))  # type: ignore[literal-required]
 class PyTreeNode:
   """Base class for dataclasses that should act like a JAX pytree node.
 
