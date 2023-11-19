85fd91eb20b5a9eee63f8fd7e833f2a65e0900f2
HMUNACHI <ndubuakuhenry@gmail.com>
2023-11-19 16:28:10 +0000

    Adds GeGLU activation and tests.

--- tests/linen/linen_activation_test.py
+++ tests/linen/linen_activation_test.py
@@ -33,6 +33,28 @@ class ActivationTest(parameterized.TestCase):
     y, _ = act.init_with_output(rng, x)
     self.assertEqual(y.shape, x.shape)
 
+  def test_geglu(self):
+    rng = random.key(0)
+    x = jnp.ones((4, 6, 5))
+    act = nn.GeGLU()
+    y, _ = act.init_with_output(rng, x)
+    self.assertEqual(y.shape, x.shape)
+
+  def test_geglu_with_dim_expansion(self):
+    rng = random.key(0)
+    x = jnp.ones((4, 6, 5))
+    act = nn.GeGLU(10)
+    expected_shape = (4, 6, 10)
+    y, _ = act.init_with_output(rng, x)
+    self.assertEqual(y.shape, expected_shape)
+
+  def test_geglu_with_dim_contraction(self):
+    rng = random.key(0)
+    x = jnp.ones((4, 6, 5))
+    act = nn.GeGLU(3)
+    expected_shape = (4, 6, 3)
+    y, _ = act.init_with_output(rng, x)
+    self.assertEqual(y.shape, expected_shape)
 
 if __name__ == '__main__':
   absltest.main()
