3dd2825f4cfbf7818e506662cfe17b3123dd912b
Henry Ndubuaku <ndubuakuhenry@gmail.com>
2023-11-20 19:48:06 +0000

    Update linen_activation_test.py
    Included various tests for GeGLU and PReLU outputs.

--- tests/linen/linen_activation_test.py
+++ tests/linen/linen_activation_test.py
@@ -33,9 +33,19 @@ class ActivationTest(parameterized.TestCase):
     y, _ = act.init_with_output(rng, x)
     self.assertEqual(y.shape, x.shape)
 
-  def test_geglu(self):
+  def test_prelu_results(self):
     rng = random.key(0)
-    x = jnp.ones((4, 6, 5))
+    x = jnp.array([[0.123,0.234], [0.456,0.789]])
+    act = nn.PReLU()
+    expected_result = jnp.array([[0.123, 0.234],[0.456, 0.789]])
+    y, _ = act.init_with_output(rng, x)
+    for row in range(expected_result.shape[0]):
+      for col in range(expected_result.shape[1]):
+        self.assertAlmostEqual(y[row,col], expected_result[row,col])
+
+  def test_geglu_high_dimension(self):
+    rng = random.key(0)
+    x = jnp.ones((4, 7, 6, 5, 2))
     act = nn.GeGLU()
     y, _ = act.init_with_output(rng, x)
     self.assertEqual(y.shape, x.shape)
@@ -50,11 +60,43 @@ class ActivationTest(parameterized.TestCase):
 
   def test_geglu_with_dim_contraction(self):
     rng = random.key(0)
-    x = jnp.ones((4, 6, 5))
+    x = jnp.ones((4, 6, 5, 7))
     act = nn.GeGLU(3)
-    expected_shape = (4, 6, 3)
+    expected_shape = (4, 6, 5, 3)
     y, _ = act.init_with_output(rng, x)
     self.assertEqual(y.shape, expected_shape)
+
+  def test_geglu_result(self):
+    rng = random.key(0)
+    x = jnp.array([[0.123,0.234], [0.456,0.789]])
+    act = nn.GeGLU()
+    expected_result = jnp.array([[0.00024275, -0.00208032],
+                                [0.00336634, -0.02307648]])
+    y, _ = act.init_with_output(rng, x)
+    for row in range(expected_result.shape[0]):
+      for col in range(expected_result.shape[1]):
+        self.assertAlmostEqual(y[row,col], expected_result[row,col])
+
+  def test_geglu_with_dim_expansion_result(self):
+    rng = random.key(0)
+    x = jnp.array([[0.123,0.234], [0.456,0.789]])
+    act = nn.GeGLU(3)
+    expected_result = jnp.array([[-0.02157649, -0.00018928, -0.01176354],
+                                [-0.08777858, 0.00258885, -0.18744925]])
+    y, _ = act.init_with_output(rng, x)
+    for row in range(expected_result.shape[0]):
+      for col in range(expected_result.shape[1]):
+        self.assertAlmostEqual(y[row,col], expected_result[row,col])
+
+  def test_geglu_with_dim_contraction_result(self):
+    rng = random.key(0)
+    x = jnp.array([[0.123,0.234], [0.456,0.789]])
+    act = nn.GeGLU(1)
+    expected_result = jnp.array([[0.00224223], [0.0307451]])
+    y, _ = act.init_with_output(rng, x)
+    for row in range(expected_result.shape[0]):
+      for col in range(expected_result.shape[1]):
+        self.assertAlmostEqual(y[row,col], expected_result[row,col])
 
 if __name__ == '__main__':
   absltest.main()
