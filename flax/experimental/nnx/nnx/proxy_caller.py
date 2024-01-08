44e96a2263526b27f267fb11f7df7dfd357bccb0
Cristian Garcia <cgarcia.e88@gmail.com>
2024-01-08 23:11:34 +0000

    [nnx] cleanup CallableProxy

--- flax/experimental/nnx/nnx/proxy_caller.py
+++ flax/experimental/nnx/nnx/proxy_caller.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import annotations
 
 import dataclasses
 import typing as tp
@@ -18,28 +19,6 @@ import typing as tp
 import typing_extensions as tpe
 
 A = tp.TypeVar('A')
-
-
-class _ProxyContext(tpe.Protocol):
-  def __call__(self, accessor: 'DelayedAccessor', /, *args, **kwargs) -> tp.Any:
-    ...
-
-
-@dataclasses.dataclass
-class CallableProxy:
-  _proxy_context: _ProxyContext
-  _proxy_callable: tp.Callable[..., tp.Any]
-
-  def __call__(self, *args, **kwargs):
-    return self._proxy_context(self._proxy_callable, *args, **kwargs)
-
-  def __getattr__(self, name) -> 'CallableProxy':
-    return CallableProxy(
-      self._proxy_context, getattr(self._proxy_callable, name)
-    )
-
-  def __getitem__(self, key) -> 'CallableProxy':
-    return CallableProxy(self._proxy_context, self._proxy_callable[key])
 
 
 def _identity(x):
@@ -58,6 +37,28 @@ class DelayedAccessor:
 
   def __getitem__(self, key):
     return DelayedAccessor(lambda x: x[key])
+
+
+class _AccessorCall(tpe.Protocol):
+  def __call__(self, accessor: DelayedAccessor, /, *args, **kwargs) -> tp.Any:
+    ...
+
+
+class CallableProxy:
+  def __init__(
+    self, callable: _AccessorCall, accessor: DelayedAccessor | None = None
+  ):
+    self._callable = callable
+    self._accessor = DelayedAccessor() if accessor is None else accessor
+
+  def __call__(self, *args, **kwargs):
+    return self._callable(self._accessor, *args, **kwargs)
+
+  def __getattr__(self, name) -> 'CallableProxy':
+    return CallableProxy(self._callable, getattr(self._accessor, name))
+
+  def __getitem__(self, key) -> 'CallableProxy':
+    return CallableProxy(self._callable, self._accessor[key])
 
 
 class ApplyCaller(tp.Protocol, tp.Generic[A]):
