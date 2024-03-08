c279f24812bb975cf060b115aef049a3443184f6
Marcus Chiam <marcuschiam@google.com>
2024-03-07 18:04:11 -0800

    removed getattr/setattr magic from nnx.Pytree

--- flax/experimental/nnx/tests/test_spmd.py
+++ flax/experimental/nnx/tests/test_spmd.py
@@ -70,10 +70,10 @@ class TestSPMD:
     )
     state_spec = nnx.get_partition_spec(state)
 
-    assert state_spec.params['w'].raw_value == PartitionSpec('row', 'col')
-    assert state_spec.opt_state[0].mu['w'].raw_value == PartitionSpec(
+    assert state_spec.params.value['w'].raw_value == PartitionSpec('row', 'col')
+    assert state_spec.opt_state.value[0].mu.value['w'].raw_value == PartitionSpec(
       'row', 'col'
     )
-    assert state_spec.opt_state[0].nu['w'].raw_value == PartitionSpec(
+    assert state_spec.opt_state.value[0].nu.value['w'].raw_value == PartitionSpec(
       'row', 'col'
     )
