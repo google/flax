c54d9b99c6ba137831d62a90c02ca44540b05ed2
Flax Team <no-reply@google.com>
2024-04-08 09:39:59 -0700

    Add **init_kwargs to self.param type annotation.
    PiperOrigin-RevId: 622872682

--- flax/linen/module.py
+++ flax/linen/module.py
@@ -1761,7 +1761,7 @@ class Module(ModuleBase):
 
   @overload
   def param(
-    self, name: str, init_fn: Callable[..., T], *init_args,
+      self, name: str, init_fn: Callable[..., T], *init_args, **init_kwargs
   ) -> T:
     ...
 
