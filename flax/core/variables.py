c58d25d3e942fa4ce88f7e25827dbc3be084a12a
Samuel <albanie@google.com>
2024-04-14 10:17:53 +0100

    Fix typos in `flax.core.variables`
    Two minor documentation fixes:
    
    1. Update the docstring for the Variable dictionary to use appropriate variable names for the batch norm statistics (currently, the two examples are `"moving_mean"` and `"moving_average"`, rather than `"moving_mean"` and `"moving_var"`).
    2. Fix code syntax highlighting for the explanatory docstring.

--- flax/core/variables.py
+++ flax/core/variables.py
@@ -27,12 +27,12 @@ For example, consider the following variable dictionary::
       "Conv2": {...}
     },
     "batch_stats": {
-      "BatchNorm1": { "moving_mean": ..., "moving_average": ...}
+      "BatchNorm1": { "moving_mean": ..., "moving_var": ...}
     }
   }
 
 In this case, the ``"BatchNorm1"`` key lives in both the ``"params"`` and
-```"batch_stats""`` collections. This reflects the fact that the submodule
+``"batch_stats""`` collections. This reflects the fact that the submodule
 named ``"BatchNorm1"`` has both trainable parameters (the ``"params"`` collection),
 as well as other non-trainable variables (the ``"batch_stats"`` collection)
 
