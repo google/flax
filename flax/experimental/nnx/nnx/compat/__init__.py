72ea9599cb371f76e5bca23129603ed0be4e674f
Cristian Garcia <cgarcia.e88@gmail.com>
2024-05-13 14:12:21 +0100

    [nnx] add compat

---
+++ flax/experimental/nnx/nnx/compat/__init__.py
@@ -0,0 +1,9 @@
+
+from .module import ModuleMeta as ModuleMeta
+from .module import Module as Module
+from .module import Scope as Scope
+from .module import compact as compact
+from .wrappers import functional as functional
+from .wrappers import LinenWrapper as LinenWrapper
+from .wrappers import Functional as Functional
+from .wrappers import NNXWrapper as NNXWrapper
