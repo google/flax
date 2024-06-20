ab8c773a3c05118db9dd06c0a6876baa1851e6d8
Silvano Bonacina <sibonaci@google.com>
2024-06-20 03:49:05 -0700

    Helper function for loading params from a linen module
    PiperOrigin-RevId: 644968776

---
+++ flax/nnx/examples/gemma/helpers.py
@@ -0,0 +1,87 @@
+# Copyright 2024 The Flax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Copyright 2024 The Flax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Helper functions."""
+
+from typing import Callable, Optional, Tuple, TypeVar
+import flax
+from flax import nnx
+from flax.typing import VariableDict  # pylint: disable=g-importing-member,g-multiple-import
+
+M = TypeVar('M', bound='nnx.Module')
+
+
+def _flatten_path(path: Tuple[str | int, ...]) -> str:
+  def f(item) -> str:
+    if isinstance(item, str):
+      return f'{item}'
+    elif isinstance(item, int):
+      return f'[{item}]'
+    else:
+      raise ValueError(f'Unexpected type {type(item)}')
+
+  return '.'.join([f(item) for item in path]).replace('.[', '[')
+
+
+def module_from_linen_variables(
+    module_factory: Callable[[], M],
+    variables: VariableDict,
+    map_key_fn: Optional[
+        Callable[[Tuple[str, ...]], Tuple[str | int, ...]]
+    ] = None,
+) -> M:
+  """Returns an `nnx.Module` initialized with the `variables` of a linen module.
+
+  Args:
+    module_factory: A no-args callable that returns an `nnx.Module`.
+    variables: A dictionary of variables.
+    map_key_fn: An optional function for mapping keys in the `variables`
+      dictionary to keys in the `nnx.Module`'s state. If not provided it is
+      assumed that after removing the collection name the keys in the
+      `variables` dictionary are the same as the keys in the `nnx.Module`'s
+      state.
+  """
+  if map_key_fn is None:
+
+    def map_key_fn(path: Tuple[str, ...]) -> Tuple[str | int, ...]:
+      return path[1:] if 'params' in variables else path
+
+  mdl: M = nnx.eval_shape(module_factory)
+  graph_def, state = nnx.split(mdl)
+  state = state.flat_state()
+  for path, val in flax.traverse_util.flatten_dict(variables).items():
+    mapped_path = map_key_fn(path)
+    if mapped_path not in state:
+      raise ValueError(
+          f"'{mdl.__class__.__name__}.{_flatten_path(mapped_path)}' doesn't "
+          f' exist (original path={path}).'
+      )
+    state[mapped_path].value = val
+  state = nnx.State.from_flat_path(state)
+
+  return nnx.merge(graph_def, state)
---
+++ flax/nnx/examples/gemma/helpers_test.py
@@ -0,0 +1,163 @@
+# Copyright 2024 The Flax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Copyright 2024 The Flax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Tests for helpers."""
+
+from typing import Tuple
+
+from absl.testing import absltest
+from absl.testing import parameterized
+from flax import nnx
+import flax.linen as nn
+from flax.nnx.examples.gemma import helpers
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+
+class ModuleFromLinenVariablesTest(parameterized.TestCase):
+
+  @parameterized.parameters(
+      dict(
+          inputs_shape=(1, 2, 3, 4),
+          num_features=10,
+          use_bias=True,
+      ),
+      dict(
+          inputs_shape=(10, 5),
+          num_features=4,
+          use_bias=False,
+      ),
+  )
+  def test_same_structure(self, inputs_shape, num_features, use_bias):
+    rng_key = jax.random.PRNGKey(0)
+    rng_inputs, rng_params = jax.random.split(rng_key)
+    inputs = jax.random.normal(rng_inputs, inputs_shape)
+
+    linen_mdl = nn.Dense(
+        features=num_features,
+        use_bias=use_bias,
+    )
+    linen_init_vars = linen_mdl.init(rng_params, jnp.zeros(inputs_shape))
+    linen_output = linen_mdl.apply(
+        linen_init_vars,
+        inputs,
+    )
+
+    mdl = helpers.module_from_linen_variables(
+        module_factory=lambda: nnx.Linear(
+            in_features=inputs_shape[-1],
+            out_features=num_features,
+            use_bias=use_bias,
+            rngs=nnx.Rngs(params=rng_params),
+        ),
+        variables=linen_init_vars,
+    )
+    output = mdl(inputs)
+
+    np.testing.assert_array_equal(output, linen_output)
+
+  @parameterized.parameters(
+      dict(
+          inputs_shape=(1, 2, 3, 4),
+          num_features=(10, 20, 7),
+          use_bias=(False, True, False),
+      ),
+  )
+  def test_different_structure(self, inputs_shape, num_features, use_bias):
+    rng_key = jax.random.PRNGKey(0)
+    rng_inputs, rng_params = jax.random.split(rng_key)
+    inputs = jax.random.normal(rng_inputs, inputs_shape)
+
+    linen_mdl = nn.Sequential([
+        nn.Sequential([
+            nn.BatchNorm(use_running_average=False),
+            nn.Dense(features=f, use_bias=b),
+        ])
+        for f, b in zip(num_features, use_bias)
+    ])
+    linen_init_vars = linen_mdl.init(rng_key, jnp.zeros(inputs_shape))
+    linen_output, linen_vars = linen_mdl.apply(
+        linen_init_vars,
+        inputs,
+        mutable=['batch_stats'],
+    )
+
+    module_factory = lambda: nnx.Sequential(*[
+        nnx.Sequential(
+            nnx.BatchNorm(
+                num_features=in_f,
+                use_running_average=False,
+                rngs=nnx.Rngs(params=rng_params),
+            ),
+            nnx.Linear(
+                in_features=in_f,
+                out_features=out_f,
+                use_bias=b,
+                rngs=nnx.Rngs(params=rng_params),
+            ),
+        )
+        for in_f, out_f, b in zip(in_features, out_features, use_bias)
+    ])
+
+    def _map_key_fn(key: Tuple[str, ...]) -> Tuple[str | int, ...]:
+      new_key = []
+      for k in key[1:]:
+        if k.startswith('layers_'):
+          prefix, suffix = k.split('layers_')
+          assert not prefix, prefix
+          new_key.append('layers')
+          new_key.append(int(suffix))
+        else:
+          new_key.append(k)
+
+      return tuple(new_key)
+
+    in_features = (inputs_shape[-1], *num_features[:-1])
+    out_features = num_features
+    mdl = helpers.module_from_linen_variables(
+        module_factory=module_factory,
+        variables=linen_init_vars,
+        map_key_fn=_map_key_fn,
+    )
+    output = mdl(inputs)
+
+    np.testing.assert_array_equal(output, linen_output)
+    for i in range(len(num_features)):
+      np.testing.assert_array_equal(
+          mdl.layers[i].layers[0].mean.value,
+          linen_vars['batch_stats'][f'layers_{i}']['layers_0']['mean'],
+      )
+      np.testing.assert_array_equal(
+          mdl.layers[i].layers[0].var.value,
+          linen_vars['batch_stats'][f'layers_{i}']['layers_0']['var'],
+      )
+
+
+if __name__ == '__main__':
+  absltest.main()
