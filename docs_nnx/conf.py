ce20ed52125098181f31ab0250a61168e9010832
Cristian Garcia <cgarcia.e88@gmail.com>
2024-10-01 12:40:09 +0100

    point nnx banner to flax-linen

--- docs_nnx/conf.py
+++ docs_nnx/conf.py
@@ -113,7 +113,7 @@ html_extra_path = ['robots.txt']
 # href with no underline and white bold text color
 announcement = """
 <a
-  href="https://flax.readthedocs.io/en/latest"
+  href="https://flax-linen.readthedocs.io/en/latest"
   style="text-decoration: none; color: white;"
 >
   This Flax NNX. Click here for <b>Flax Linen</b>.
