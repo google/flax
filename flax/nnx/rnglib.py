011b69fd34dc3a1c728c923882f1cdb2ca97923c
Cristian Garcia <cgarcia.e88@gmail.com>
2025-08-25 18:57:15 -0700

    fix mypy

--- flax/nnx/rnglib.py
+++ flax/nnx/rnglib.py
@@ -840,7 +840,7 @@ def split_rngs(
       if squeeze:
         key = key[0]
       if variablelib.is_array_ref(stream.key.raw_value):
-        stream.key.raw_value = variablelib.array_ref(key)
+        stream.key.raw_value = variablelib.array_ref(key)  # type: ignore[assignment]
       else:
         stream.key.value = key
       if squeeze:
@@ -852,7 +852,7 @@ def split_rngs(
 
       count = jnp.zeros(counts_shape, dtype=jnp.uint32)
       if variablelib.is_array_ref(stream.count.raw_value):
-        stream.count.raw_value = variablelib.array_ref(count)
+        stream.count.raw_value = variablelib.array_ref(count)  # type: ignore[assignment]
       else:
         stream.count.value = count
 
